d884bfc76bf906833b245cff6558a688b03335c1
riopwg <riopwg@piwigo.org>
2009-08-23 21:53:20 +0000

    Update lang IT
    git-svn-id: http://piwigo.org/svn/extensions/ContactForm@3789 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/it_IT/plugin.lang.php
+++ language/it_IT/plugin.lang.php
@@ -4,43 +4,43 @@ if (!defined('PHPWG_ROOT_PATH')) die('Hacking attempt!');
 global $lang;
 
 $lang['cf_plugin_name'] = 'Contact Form';
-// TODO $lang['contact_form_debug'] = '';
+$lang['contact_form_debug'] = 'Visualizzare le informazioni di debug';
 
 // ==================================================================
 // Default values if not configured
-// TODO $lang['contact_form_title'] = ''; 
-// TODO $lang['contact_form'] = '';
+$lang['contact_form_title'] = 'Formulario di contatto';
+$lang['contact_form'] = 'Contattare';
 $lang['contact_form_link'] = 'Contattare il webmaster';
 
 // ==================================================================
 // Redirect page
-// TODO $lang['contact_redirect_title'] = '';
+$lang['contact_redirect_title'] = 'Stato d\'invio messaggio';
 
 // ==================================================================
 // Menubar block
-// TODO $lang['cf_from_name'] = ''; 
-// TODO $lang['cf_from_mail'] = ''; 
-// TODO $lang['cf_subject'] = ''; 
-// TODO $lang['cf_message'] = ''; 
-// TODO $lang['cf_submit'] = ''; 
+$lang['cf_from_name'] = 'Il vostro nome';
+$lang['cf_from_mail'] = 'La vostra E-mail';
+$lang['cf_subject'] = 'Soggetto';
+$lang['cf_message'] = 'Messaggio';
+$lang['cf_submit'] = 'Inviare';
 
 // ==================================================================
 // Messages
-// TODO $lang['cf_from_name_error'] = ''; 
+$lang['cf_from_name_error'] = 'Entrare un nome';
 $lang['cf_mail_format_error'] = $lang['reg_err_mail_address'];
-// TODO $lang['cf_subject_error'] = ''; 
-// TODO $lang['cf_message_error'] = ''; 
-// TODO $lang['cf_error_sending_mail'] = ''; 
-// TODO $lang['cf_sending_mail_successful'] = '';
-// TODO $lang['cf_form_error'] = ''; 
+$lang['cf_subject_error'] = 'Entrare un soggetto';
+$lang['cf_message_error'] = 'Entrare un messaggio';
+$lang['cf_error_sending_mail'] = 'Errore durante l\'invio dell\'E-mail'; 
+$lang['cf_sending_mail_successful'] = 'Invio dell\'E-mail riuscito';
+TODO $lang['cf_form_error'] = 'Dati errati'; 
 $lang['cf_inconsistent_version'] = '%s : versioni incoerenti';
 $lang['cf_no_unlink'] = 'La funzione \'unlink\' non è disponibile';
 $lang['cf_unlink_errors'] = 'Errori durante la sopressione dei file';
 $lang['cf_config_saved'] = 'Configurazione salvata con successo';
 $lang['cf_config_saved_with_errors'] = 'Configurazione salvata con errori';
-// TODO $lang['cf_length_not_integer'] = '';
-// TODO $lang['cf_delay_not_integer'] = '';
-//TODO $lang['cf_link_error'] = '';
+$lang['cf_length_not_integer'] = 'Le dimensioni devono essere un numero intero';
+$lang['cf_delay_not_integer'] = 'Il limite deve essere un numero intero';
+$lang['cf_link_error'] = 'La variabile non può contenere degli spazi';
 
 // ==================================================================
 // Admin page
@@ -50,35 +50,34 @@ $lang['cf_tab_config'] = 'Configurazione';
 $lang['cf_config'] = 'Configurazione';
 $lang['cf_config_desc'] = 'Configurazione principale del plugin';
 $lang['cf_label_config'] = 'Configurazione generale';
-// TODO $lang['cf_label_mail'] = '';
-// TODO $lang['cf_menu_link'] = '';
-// TODO $lang['cf_guest_allowed'] = '';
-// TODO $lang['cf_mail_prefix'] = '';
-// TODO $lang['cf_separator'] = '';
-// TODO $lang['cf_separator_length'] = '';
-// TODO $lang['cf_mandatory_name'] = '';
-// TODO $lang['cf_mandatory_mail'] = '';
-// TODO $lang['cf_redirect_delay'] = '';
-// TODO $lang['cf_label_link'] = '';
-// TODO $lang['cf_define_link'] = '';
-// TODO $lang['cf_link'] = '';
+TODO $lang['cf_label_mail'] = 'Configurazione dell\'E-mail';
+$lang['cf_menu_link'] = 'Aggiungere un link nel menu';
+$lang['cf_guest_allowed'] = 'Autorizzare gli ospiti ad accedere al formulario';
+$lang['cf_mail_prefix'] = 'Prefisso dell\'E-mail inviata';
+$lang['cf_separator'] = 'Carattere(i) usato(i) per definire una barra di separazione nella E-mail al formato testo';
+$lang['cf_separator_length'] = 'Dimenzioni della barra di separazione';
+$lang['cf_mandatory_name'] = 'Nome obbligatorio';
+$lang['cf_mandatory_mail'] = 'E-mail obbligatoria';
+$lang['cf_redirect_delay'] = 'Limite d\'attesa ';
+$lang['cf_label_link'] = 'Gestione del link (escluso quello del menu) verso il formulario';
+$lang['cf_define_link'] = 'Definire il link';
+$lang['cf_link'] = 'Nome della variabile del template contenente il link HTML verso il formulario di contatto';
 // Language tab
-// TODO $lang['cf_tab_language'] = '';
-// TODO $lang['cf_language_desc'] = ' ';
-// TODO $lang['cf_select_item'] = ' ';
+$lang['cf_tab_language'] = 'Localizzazione';
+$lang['cf_language_desc'] = 'Traduzzione dei messaggi';
+$lang['cf_select_item'] = 'Selezzionare l\'elemento da tradurre';
 $lang['cf_default_lang'] = 'Di default';
-// TODO $lang['contact_form_title_label'] = ' ';
-// TODO $lang['contact_form_label'] = ' ';
-// TODO $lang['contact_form_link_label'] = ' ';
+$lang['contact_form_title_label'] = 'Titolo nella barra dei menu';
+$lang['contact_form_label'] = 'Nome visualizzato nella barra dei menu';
+$lang['contact_form_link_label'] = 'Testo utilizzato per il link di contatto nel footer';
 // History tab
-// TODO $lang['cf_tab_history'] = '';
-// TODO $lang['cf_history'] = '';
-// TODO $lang['cf_history_desc'] = '';
-// TODO $lang['cf_history_date'] = '';
-// TODO $lang['cf_history_version'] = '';
-// TODO $lang['cf_history_log'] = '';
-// TODO $lang['cf_file_not_found'] = '';
-// TODO $lang['cf_file_empty'] = '';
-// TODO $lang['cf_format_date'] = '%D %M %Y';
-
+$lang['cf_tab_history'] = 'Cronologia';
+$lang['cf_history'] = 'Cronologia';
+$lang['cf_history_desc'] = 'Cronologia delle modifiche';
+$lang['cf_history_date'] = 'Data';
+$lang['cf_history_version'] = 'Versione';
+$lang['cf_history_log'] = 'Changelog';
+$lang['cf_file_not_found'] = 'File non trovato';
+$lang['cf_file_empty'] = 'File vuoto';
+$lang['cf_format_date'] = '%D %M %Y';
 ?>
