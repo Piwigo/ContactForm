a65442cc462ef1ed2a0cd6857ce9b177c43d0227
Nakre <Nakre@piwigo.org>
2011-02-02 13:15:54 +0000

    cleaning / updating translates..
    git-svn-id: http://piwigo.org/svn/extensions/ContactForm@9058 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/tr_TR/plugin.lang.php
+++ language/tr_TR/plugin.lang.php
@@ -2,53 +2,53 @@
 if (!defined('PHPWG_ROOT_PATH')) die('Hacking attempt!');
 global $lang;
 
-$lang['cf_plugin_name'] = 'İletiş Formu';
+$lang['cf_plugin_name'] = 'İletişim Formu';
 $lang['contact_form_debug'] = 'Debug bilgisini gör';
 
 // ==================================================================
 // Default values if not configured
 $lang['contact_form_title'] = 'İletişim formu';
 $lang['contact_form'] = 'İletişim';
-$lang['contact_form_link'] = 'İletişim- Site yöneticisi';
+$lang['contact_form_link'] = 'İletişim Site yöneticisi';
 
 // ==================================================================
 // Redirect page
 $lang['contact_redirect_title'] = 'Mesajın durumunu gönder';
 
 // ==================================================================
-// Menubar block
+// Menü bloğu
 $lang['cf_from_name'] = 'İsminiz';
-$lang['cf_from_mail'] = 'e-posta adresiniz';
+$lang['cf_from_mail'] = 'E-posta adresiniz';
 $lang['cf_subject'] = 'Konu';
 $lang['cf_message'] = 'Mesaj';
 $lang['cf_submit'] = 'Gönder';
 $lang['title_send_mail'] = '';
 
 // ==================================================================
-// Messages
+// Mesajlar
 $lang['cf_from_name_error'] = 'Lütfen isminizi girin';
-$lang['cf_mail_format_error'] = $lang['mail address must be like xxx@yyy.eee (example : jack@altern.org)'];
-$lang['cf_subject_error'] = 'Lütgen konuyu girin';
+$lang['cf_mail_format_error'] = $lang['e-posta şöyle olmalıdır xxx@yyy.eee (örnek : jack@altern.org)'];
+$lang['cf_subject_error'] = 'Lütfen konuyu girin';
 $lang['cf_message_error'] = 'Lütfen mesajınızı girin';
-$lang['cf_error_sending_mail'] = 'Mesajın görilmesinde sorun oluştu';
+$lang['cf_error_sending_mail'] = 'Mesajın gönderilmesinde sorun oluştu';
 $lang['cf_sending_mail_successful'] = 'Mesajınız başarılı bir şekilde gönderildi.';
 $lang['cf_form_error'] = 'Geçersiz bilgi';
 $lang['cf_no_unlink'] = 'Fonksiyon \'unlink\' ulaşılamaz...';
 $lang['cf_unlink_errors'] = 'Dosyada hata oluştu';
-$lang['cf_config_saved'] = 'Değişiklikler başarılı bir şekilde kaydedildi.';
-$lang['cf_config_saved_with_errors'] = 'Değişikler hatalı bir şlde kaydedildi';
-$lang['cf_length_not_integer'] = 'Büyüklük değeri tam sayı olmalı';
+$lang['cf_config_saved'] = 'Değişiklikler başarılı ile kaydedildi.';
+$lang['cf_config_saved_with_errors'] = 'Değişikler hatalar ile kaydedildi';
+$lang['cf_length_not_integer'] = 'Boyut değeri tam sayı olmalı';
 $lang['cf_delay_not_integer'] = 'Geçikme değeri tam sayı olmalı';
-$lang['cf_link_error'] = 'Değişkeler bu alanı kapsayamaz';
+$lang['cf_link_error'] = 'Değişkenler boşluk içeremez';
 $lang['cf_hide'] = 'Gizle';
 
 // ==================================================================
-// Admin page
+// Yönetici Sayfası
 $lang['cf_validate'] = 'Gönder';
-// Configuration tab
+// Ayarlar  bölümü
 $lang['cf_tab_config'] = 'Ayarlar';
 $lang['cf_config'] = 'Ayarlar';
-$lang['cf_config_desc'] = 'Plugin ana ayarlar';
+$lang['cf_config_desc'] = 'Eklenti ayarları';
 $lang['cf_label_config'] = 'Genel ayarlar';
 $lang['cf_label_mail'] = 'E-posta ayarları';
 $lang['cf_menu_link'] = 'Menüye link ekle';
@@ -62,20 +62,20 @@ $lang['cf_redirect_delay'] = 'Geribildirimde geçikme süresi';
 $lang['cf_label_link'] = 'Link yönetimi (dışardan menü)';
 $lang['cf_define_link'] = 'Adresi belirt';
 $lang['cf_link'] = 'Name of the template variable containing the HTML link to the contact form';
-// Emails tab
+// E-posta  bölümü
 $lang['cf_tab_emails'] = 'E-postalar';
 $lang['cf_emails'] = 'E-postalar';
-$lang['cf_emails_desc'] = 'Destination e-mails management';
-$lang['cf_active'] = 'Active e-mail';
-$lang['cf_no_mail'] = 'No e-mail address available';
-$lang['cf_refresh'] = 'Regenerate e-mail list address';
-// Language tab
-$lang['cf_tab_language'] = 'Localization';
-$lang['cf_language'] = 'Localization';
-$lang['cf_language_desc'] = 'Messages translation';
-$lang['cf_select_item'] = 'Select item to translate';
-$lang['cf_default_lang'] = 'Default';
-$lang['contact_form_title_label'] = 'Title used in menubar';
-$lang['contact_form_label'] = 'Name used in menubar';
-$lang['contact_form_link_label'] = 'Text used for contact link in the page footer';
+$lang['cf_emails_desc'] = 'Hedef e-posta yönetimi';
+$lang['cf_active'] = 'Etkin e-posta';
+$lang['cf_no_mail'] = 'Mevcut e-posta adresi yok';
+$lang['cf_refresh'] = 'E-posta listesini tekrar oluştur';
+// Dil bölümü
+$lang['cf_tab_language'] = 'Yerelleştirme';
+$lang['cf_language'] = 'Yerelleştirme';
+$lang['cf_language_desc'] = 'Mesajları çevir';
+$lang['cf_select_item'] = 'Çevirmek için öğe seçiniz';
+$lang['cf_default_lang'] = 'Varsayılan';
+$lang['contact_form_title_label'] = 'Menüde kullanılan başlık';
+$lang['contact_form_label'] = 'Menüde kullanılan isim';
+$lang['contact_form_link_label'] = 'İletişim bağlantısı altında kullanılacak yazı';
 ?>
