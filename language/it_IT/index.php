293bf56663fccb264d8ac4f573ecf6ddce66cdc3
riopwg <riopwg@piwigo.org>
2011-06-27 14:14:13 +0000

    Update index.php file in language IT DIR
    git-svn-id: http://piwigo.org/svn/extensions/ContactForm@11529 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/it_IT/index.php
+++ language/it_IT/index.php
@@ -1,13 +1,10 @@
 <?php
 // +-----------------------------------------------------------------------+
-// | PhpWebGallery - a PHP based picture gallery                           |
-// | Copyright (C) 2002-2003 Pierrick LE GALL - pierrick@phpwebgallery.net |
-// | Copyright (C) 2003-2007 PhpWebGallery Team - http://phpwebgallery.net |
+// | Piwigo - a PHP based photo gallery                                    |
 // +-----------------------------------------------------------------------+
-// | file          : $Id: index.php,v 1.1 2009/08/13 12:39:52 Criss Exp $
-// | last update   : $Date: 2009/08/13 12:39:52 $
-// | last modifier : $Author: Criss $
-// | revision      : $Revision: 1.1 $
+// | Copyright(C) 2008-2011 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
 // +-----------------------------------------------------------------------+
 // | This program is free software; you can redistribute it and/or modify  |
 // | it under the terms of the GNU General Public License as published by  |
