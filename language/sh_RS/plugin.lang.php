f03e3c8e7d0e52c24aba8e5b18e5fab166144878
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-04-03 18:33:58 +0000

    [ContactForm] Add sh_RS, thanks to : vladamilanovic
    git-svn-id: http://piwigo.org/svn/extensions/ContactForm@13876 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/sh_RS/plugin.lang.php
@@ -0,0 +1,70 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['cf_active'] = 'Aktivni e-mejl';
+$lang['cf_config'] = 'Podešavanje';
+$lang['cf_config_desc'] = 'Glavno podešavanje dodatka';
+$lang['cf_config_saved'] = 'Podešavanje uspešno sačuvano';
+$lang['cf_config_saved_with_errors'] = 'Podešavanje sačuvano sa greškama';
+$lang['cf_delay_not_integer'] = 'Kašnjenje mora biti ceo broj';
+$lang['cf_emails'] = 'E-mejlovi';
+$lang['cf_emails_desc'] = 'Upravljanje odredišnim e-mejlovima';
+$lang['cf_error_sending_mail'] = 'Greška prilikom slanja e-mejla';
+$lang['cf_form_error'] = 'Nepravilni podaci';
+$lang['cf_from_mail'] = 'Vaš e-mejl';
+$lang['cf_from_name'] = 'Vaše ime';
+$lang['cf_from_name_error'] = 'Molim unesite ime';
+$lang['cf_guest_allowed'] = 'Dozvoli gostima da vide formu';
+$lang['cf_hide'] = 'Sakrij';
+$lang['cf_label_config'] = 'Opšte podešavanje';
+$lang['cf_label_mail'] = 'Podešavanje e-mejla';
+$lang['cf_length_not_integer'] = 'Veličina mora biti ceo broj';
+$lang['cf_link_error'] = 'Promenljiva ne sme sadržati razmake';
+$lang['cf_mail_format_error'] = 'Pogrešan format e-mejla';
+$lang['cf_mail_prefix'] = 'Prefiks naslova poslatih e-mejlova';
+$lang['cf_mandatory_mail'] = 'E-mejl adresa je obavezna';
+$lang['cf_mandatory_name'] = 'Ime je obavezno';
+$lang['cf_menu_link'] = 'Dodaj vezu u meni';
+$lang['cf_message'] = 'Poruka';
+$lang['cf_message_error'] = 'Molim unesite poruku';
+$lang['cf_no_mail'] = 'Nema dostupnih e-mejl adresa';
+$lang['cf_no_unlink'] = 'Funkcija \'razveži\' nije dostupna';
+$lang['cf_plugin_name'] = 'Kontakt forma';
+$lang['cf_redirect_delay'] = 'Pauziraj kašnjenje preusmeravanja';
+$lang['cf_refresh'] = 'Regeneriši listu e-mejl adresa';
+$lang['cf_sending_mail_successful'] = 'E-mail uspešno poslat ';
+$lang['cf_separator'] = 'Karakter(i) korišćeni za definisanje linije odvajanja u formatu teksta e-maila';
+$lang['cf_separator_length'] = 'Veličina linije';
+$lang['cf_subject'] = 'Naslov';
+$lang['cf_subject_error'] = 'Molim unesite naslov';
+$lang['cf_submit'] = 'Pošalji';
+$lang['cf_tab_config'] = 'Podešavanje';
+$lang['cf_tab_emails'] = 'E-mejlovi';
+$lang['cf_unlink_errors'] = 'Greška prilikom brisanja fajla';
+$lang['cf_validate'] = 'Pošalji';
+$lang['contact_form'] = 'Kontakt';
+$lang['contact_form_debug'] = 'Prikaži debag informacije';
+$lang['contact_form_link'] = 'Kontakt vebmaster';
+$lang['contact_form_title'] = 'Kontakt forma';
+$lang['contact_redirect_title'] = 'Pošalji poruku statusa';
+$lang['title_send_mail'] = 'Komentar sajta';
+?>
