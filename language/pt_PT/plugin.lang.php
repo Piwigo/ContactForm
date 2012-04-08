801592e318d309723c5cf6db13ca9a0a938a8f01
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-04-08 20:40:54 +0000

    [ContactForm] Add pt_PT, thanks to : PsyKick_RuhYn
    git-svn-id: http://piwigo.org/svn/extensions/ContactForm@14012 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/pt_PT/plugin.lang.php
@@ -0,0 +1,70 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['cf_active'] = 'Activar email';
+$lang['cf_config'] = 'Configuração';
+$lang['cf_config_desc'] = 'Configuração principal da extensão';
+$lang['cf_config_saved'] = 'Configuração salva com sucesso';
+$lang['cf_config_saved_with_errors'] = 'Configuração salva com erros';
+$lang['cf_delay_not_integer'] = 'Atraso deve ser um valor integer';
+$lang['cf_emails'] = 'Emails';
+$lang['cf_emails_desc'] = 'Gestão de emails de destino';
+$lang['cf_error_sending_mail'] = 'Erro ao enviar email';
+$lang['cf_form_error'] = 'Dados inválidos';
+$lang['cf_from_mail'] = 'O seu email';
+$lang['cf_from_name'] = 'O seu nome';
+$lang['cf_from_name_error'] = 'Por favor introduza um nome';
+$lang['cf_guest_allowed'] = 'Permitir visualização do formulário por convidados';
+$lang['cf_hide'] = 'Ocultar';
+$lang['cf_label_config'] = 'Configuração Geral';
+$lang['cf_label_mail'] = 'Configuração de email';
+$lang['cf_length_not_integer'] = 'Tamanho deve ser um valor integer';
+$lang['cf_link_error'] = 'Variáveis não podem conter espaços';
+$lang['cf_mail_format_error'] = 'Por favor introduza um email';
+$lang['cf_mail_prefix'] = 'Prefixo do assunto de emails enviados';
+$lang['cf_mandatory_mail'] = 'Endereço de email é obrigatório';
+$lang['cf_mandatory_name'] = 'Nome é obrigatório';
+$lang['cf_menu_link'] = 'Adicionar ligação no menú';
+$lang['cf_message'] = 'Mensagem';
+$lang['cf_message_error'] = 'Por favor introduza uma mensagem';
+$lang['cf_no_mail'] = 'Nenhum endereço de email disponível';
+$lang['cf_no_unlink'] = 'Função \'unlink\' não disponível';
+$lang['cf_plugin_name'] = 'Formulário de Contacto';
+$lang['cf_redirect_delay'] = 'Atraso da pausa de redireccionamento';
+$lang['cf_refresh'] = 'Regenerar lista de endereços de email';
+$lang['cf_sending_mail_successful'] = 'Email enviado com sucesso';
+$lang['cf_separator'] = 'Caractére(s) usado(s) para definir a bara de separação no email em formato de texto';
+$lang['cf_separator_length'] = 'Tamanho da barra';
+$lang['cf_subject'] = 'Assunto';
+$lang['cf_subject_error'] = 'Por favor introduza um assunto';
+$lang['cf_submit'] = 'Enviar';
+$lang['cf_tab_config'] = 'Configuração';
+$lang['cf_tab_emails'] = 'Emails';
+$lang['cf_unlink_errors'] = 'Ocorreu um erro ao apagar ficheiro';
+$lang['cf_validate'] = 'Submeter';
+$lang['contact_form'] = 'Contactos';
+$lang['contact_form_debug'] = 'Mostrar informação de depuração';
+$lang['contact_form_link'] = 'Contactar Webmaster';
+$lang['contact_form_title'] = 'Formulário de Contacto';
+$lang['contact_redirect_title'] = 'Estado de envio de mensagem';
+$lang['title_send_mail'] = 'Um comentário sobre o site';
+?>
