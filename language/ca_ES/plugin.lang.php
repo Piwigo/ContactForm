009ea5983e09e3786d7fb960ecf5030f3a748cf6
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-04-27 09:34:45 +0000

    [contactform] Update ca_ES, thanks to : AlbertParera
    git-svn-id: http://piwigo.org/svn/extensions/ContactForm@14466 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/ca_ES/plugin.lang.php
@@ -0,0 +1,77 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Add text above'] = 'Afegir el text per sobre';
+$lang['Add text below'] = 'Afegir el text per sota';
+$lang['Add text to the contact form'] = 'Afegir text al formulari de contacte';
+$lang['cf_active'] = 'Correu electrònic actiu';
+$lang['cf_config_desc'] = 'Configuració general del complement';
+$lang['cf_config_saved_with_errors'] = 'Configuració desada però amb errors';
+$lang['cf_config_saved'] = 'Configuració desada correctament';
+$lang['cf_config'] = 'Configuració';
+$lang['cf_delay_not_integer'] = 'El retard ha de ser un nombre enter';
+$lang['cf_emails_desc'] = 'Gestió de destinacions dels missatges de correu electrònic';
+$lang['cf_emails'] = 'Correus electrònics';
+$lang['cf_error_sending_mail'] = 'Error a l\'enviar el correu electrònic';
+$lang['cf_form_error'] = 'Dades no vàlides';
+$lang['cf_from_mail'] = 'Correu electrònic';
+$lang['cf_from_name_error'] = 'Si us plau, introdueix un nom';
+$lang['cf_from_name'] = 'Nom';
+$lang['cf_guest_allowed'] = 'Permetre que els convidats vegin el formulari';
+$lang['cf_hide'] = 'Ocultar';
+$lang['cf_label_config'] = 'Configuració general';
+$lang['cf_label_mail'] = 'Configuració del correu electrònic';
+$lang['cf_length_not_integer'] = 'La mida ha de ser un nombre enter';
+$lang['cf_link_error'] = 'La variable no pot contenir espais';
+$lang['cf_mail_format_error'] = 'adreça electrònica ha de ser xxx@yyy.eee (exemple: jack@altern.org)';
+$lang['cf_mail_prefix'] = 'Prefix de l\'assumpte del correu electrònic enviat';
+$lang['cf_mandatory_mail'] = 'El correu electrònic és obligatori';
+$lang['cf_mandatory_name'] = 'El nom és obligatori';
+$lang['cf_menu_link'] = 'Afegeix un enllaç al menú';
+$lang['cf_message_error'] = 'Si us plau, escribiu el missatge';
+$lang['cf_message'] = 'Missatge';
+$lang['cf_no_mail'] = 'No hi ha cap adreça de correu electrònic disponible';
+$lang['cf_no_unlink'] = 'La funció \'desenllaçar\' no està disponible ...';
+$lang['cf_plugin_name'] = 'Formulari de contacte';
+$lang['cf_redirect_delay'] = 'Pausar el retard de la redirecció';
+$lang['cf_refresh'] = 'Generar llista d\'adreces de correu electrònic';
+$lang['cf_sending_mail_successful'] = 'El correu electrònic s\'ha enviat correctament';
+$lang['cf_separator_length'] = 'Mida de la barra';
+$lang['cf_separator'] = 'Caràcter(s) utilitzat per definir una barra de separació en el correu electrònic en format de text';
+$lang['cf_subject_error'] = 'Si us plau, introdueix l\'assumpte';
+$lang['cf_subject'] = 'Assumpte';
+$lang['cf_submit'] = 'Enviar';
+$lang['cf_tab_config'] = 'Configuració';
+$lang['cf_tab_emails'] = 'Correus electrònics';
+$lang['cf_tab_persoform'] = 'Afegeix formulari de text';
+$lang['cf_unlink_errors'] = 'S\'ha produït un error durant l\'eliminació d\'arxius';
+$lang['cf_validate'] = 'Enviar';
+$lang['contact_form_debug'] = 'Visualitzar informació de depuració';
+$lang['contact_form_link'] = 'Contactar amb el webmaster';
+$lang['contact_form_title'] = 'Formulari de contacte';
+$lang['contact_form'] = 'Contacte';
+$lang['contact_redirect_title'] = 'Enviar missatge d\'estat';
+$lang['if plugin ExtendedDescription is actif, you can use tags [lang]'] = 'si el complement ExtendedDescription és actiu, pots utilitzar les etiquetes [idioma]';
+$lang['title_send_mail'] = 'Hi ha un comentari al web';
+$lang['use tag < div id="persoformbottom" > to customize your personal text'] = 'fer servir l\'etiqueta <div id="persoformbottom"> per personalitzar un text';
+$lang['use tag < div id="persoformtop" > to customize your personal text'] = 'fer servir l\'etiqueta <div id = "persoformtop"> per personalitzar un text';
+?>
