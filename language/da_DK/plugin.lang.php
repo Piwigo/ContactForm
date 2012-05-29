675afe9859df8098f8fecee71e84e95e856c25e7
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-05-29 15:54:31 +0000

    [contactform] Update da_DK, thanks to : thomas1956
    git-svn-id: http://piwigo.org/svn/extensions/ContactForm@15420 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/da_DK/plugin.lang.php
@@ -0,0 +1,73 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Text after the contact form'] = 'Tekst efter kontakt formular';
+$lang['Text before the contact form'] = 'Tekst før kontakt formular';
+$lang['cf_active'] = 'Aktiv e-mail adresse';
+$lang['cf_config_desc'] = 'Grundindstillinger for plugin';
+$lang['cf_config_saved_with_errors'] = 'Indstillinger gemt, men med fejl';
+$lang['cf_config_saved'] = 'Indstillinger gemt';
+$lang['cf_config'] = 'Indstillinger';
+$lang['cf_delay_not_integer'] = 'Forsinkelse skal angives som et heltal';
+$lang['cf_emails_desc'] = 'Administration af modtager e-mails';
+$lang['cf_emails'] = 'E-mails';
+$lang['cf_error_sending_mail'] = 'Fejl ved afsendelse af e-mail';
+$lang['cf_form_error'] = 'Fejl i data';
+$lang['cf_from_mail'] = 'Din e-mail';
+$lang['cf_from_name_error'] = 'Angiv et navn';
+$lang['cf_from_name'] = 'Dit navn';
+$lang['cf_guest_allowed'] = 'Tillad gæster at se formular';
+$lang['cf_hide'] = 'Skjul';
+$lang['cf_label_config'] = 'Generelle indstillinger';
+$lang['cf_label_mail'] = 'Indstillinger for e-mail';
+$lang['cf_length_not_integer'] = 'Størrelse skal angives som et heltal';
+$lang['cf_link_error'] = 'Variabel kan ikke indeholde mellemrum';
+$lang['cf_mail_format_error'] = 'E-mail adresse skal angives som xxx@yyy.zz (fx jens@jensen.dk)';
+$lang['cf_mail_prefix'] = 'Præfix for emnefeltet i den sendte e-mail';
+$lang['cf_mandatory_mail'] = 'E-mail adresse er påkrævet';
+$lang['cf_mandatory_name'] = 'Navn er påkrævet';
+$lang['cf_menu_link'] = 'Tilføj link i menu';
+$lang['cf_message_error'] = 'Angiv en beskred';
+$lang['cf_message'] = 'Besked';
+$lang['cf_no_mail'] = 'Ingen e-mail adresse er tilgængelig';
+$lang['cf_no_unlink'] = 'Funktionen "unlink" er ikke tilgængelig';
+$lang['cf_plugin_name'] = 'Kontakt Formular';
+$lang['cf_redirect_delay'] = 'Stop forsinkelse på viderestilling';
+$lang['cf_refresh'] = 'Opdater e-mail adresseliste ';
+$lang['cf_sending_mail_successful'] = 'E-mail sendt';
+$lang['cf_separator_length'] = 'Længde på separatorlinie';
+$lang['cf_separator'] = 'Karakter(er) der anvendes som separatorlinie for e-mails i tekst format';
+$lang['cf_subject_error'] = 'Angiv et emne';
+$lang['cf_subject'] = 'Emne';
+$lang['cf_submit'] = 'Send';
+$lang['cf_tab_config'] = 'Indstillinger';
+$lang['cf_tab_emails'] = 'E-mails';
+$lang['cf_tab_persoform'] = 'Tilføj speciel tekst';
+$lang['cf_unlink_errors'] = 'Fejl ved sletning af fil';
+$lang['cf_validate'] = 'Send';
+$lang['contact_form_debug'] = 'Vis debug information';
+$lang['contact_form_link'] = 'Kontakt webmaster';
+$lang['contact_form_title'] = 'Kontakt formular';
+$lang['contact_form'] = 'Kontakt';
+$lang['contact_redirect_title'] = 'Status for send besked';
+$lang['title_send_mail'] = 'En kommentar på sitet';
+?>
