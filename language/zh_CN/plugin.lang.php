8f1b8fd354a6620a40b43dd6cbe44e10af0c89bb
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-08-17 19:59:00 +0000

    [contactform] Update zh_CN, thanks to : dennisyan
    git-svn-id: http://piwigo.org/svn/extensions/ContactForm@17594 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/zh_CN/plugin.lang.php
@@ -0,0 +1,50 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Please enter an e-mail'] = '请输入email地址';
+$lang['Prefix of the sent e-mail subject (you can use "%gallery_title%")'] = 'email标题的前缀 (您可以使用 "%gallery_title%")';
+$lang['Subject'] = '标题';
+$lang['Text after the contact form'] = '联络表单之后的文字';
+$lang['Text before the contact form'] = '联络表单之前的文字';
+$lang['Your e-mail'] = '您的email';
+$lang['Your name'] = '您的名字';
+$lang['%s must not be less than %d characters long'] = '%s 不能少于 %d 个字符';
+$lang['%s must not be more than %d characters long'] = '%s 不能多于 %d 个字符';
+$lang['+ Add an email'] = '+ 添加一个email';
+$lang['Active'] = '激活';
+$lang['Add link in menu'] = '在菜单中添加链接';
+$lang['Allow guests to see the form'] = '对访客身份可见';
+$lang['Default e-mail subject'] = '默认email标题';
+$lang['E-mail address is mandatory'] = 'email地址为必填项';
+$lang['E-mail configuration'] = 'email设置';
+$lang['E-mail format :'] = 'email格式';
+$lang['E-mail sent successfully'] = 'email发送成功';
+$lang['Error while sending e-mail'] = '发送email时出现错误';
+$lang['General configuration'] = '一般设置';
+$lang['Message'] = '内容';
+$lang['Name is mandatory'] = '名字为必填项';
+$lang['No active email address'] = '无激活的email地址';
+$lang['Plain text'] = '纯文本';
+$lang['Please enter a message'] = '请输入内容';
+$lang['Please enter a name'] = '请输入名字';
+$lang['Please enter a subject'] = '请输入标题';
+?>
