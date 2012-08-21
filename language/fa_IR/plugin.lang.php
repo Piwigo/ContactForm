e6bcdf35076234878adeee8273b13388301c4460
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-08-21 06:33:03 +0000

    [contactform] Update fa_IR, thanks to : Garcia
    git-svn-id: http://piwigo.org/svn/extensions/ContactForm@17608 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/fa_IR/plugin.lang.php
@@ -0,0 +1,50 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['%s must not be less than %d characters long'] = '%s نباید از %d حرف کمتر باشد';
+$lang['%s must not be more than %d characters long'] = '%s نباید از %d حرف بیشتر باشد';
+$lang['Active'] = 'فعال کردن';
+$lang['Add link in menu'] = 'افزودن پیوند در منو';
+$lang['Allow guests to see the form'] = 'مهمان ها بتوانند فرم را ببینند';
+$lang['E-mail address is mandatory'] = 'وارد کردن ایمیل الزامی است';
+$lang['E-mail configuration'] = 'تنظیمات ایمیل';
+$lang['E-mail format :'] = 'فرمت ایمیل :';
+$lang['E-mail sent successfully'] = 'ایمیل با موفقیت فرستاده شد';
+$lang['Error while sending e-mail'] = 'خطا در هنگام فرستادن ایمیل';
+$lang['General configuration'] = 'تنظیمات کلی';
+$lang['Name is mandatory'] = 'وارد کردن نام الزامی است';
+$lang['No active email address'] = 'هیچ آدرس ایمیل فعال شده ای موجود نیست';
+$lang['Plain text'] = 'متن ساده';
+$lang['Please enter a message'] = 'پیام خود را وارد کنید';
+$lang['Please enter a name'] = 'نام خود را وارد کنید';
+$lang['Please enter a subject'] = 'موضوع را وارد کنید';
+$lang['Please enter an e-mail'] = 'ایمیل خود را وارد کنید';
+$lang['Prefix of the sent e-mail subject (you can use "%gallery_title%")'] = 'پیشوند موضوع ایمیل (می توانید از "%gallery_title%" استفاده کنید)';
+$lang['Text after the contact form'] = 'متنِ پس از فرمِ تماس';
+$lang['Text before the contact form'] = 'متنِ پیش از فرمِ تماس';
+$lang['Your e-mail'] = 'ایمیل شما';
+$lang['Your name'] = 'نام شما';
+$lang['Subject'] = 'موضوع';
+$lang['Message'] = 'پیام';
+$lang['Default e-mail subject'] = 'موضوع پیشفرض ایمیل';
+$lang['+ Add an email'] = '+ افزودن یک ایمیل';
+?>
