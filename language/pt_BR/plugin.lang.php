e9669e499083568f396ce62fd19237c48ac89778
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-11-20 18:33:12 +0000

    [contactform] Add pt_BR, thanks to : glaucio
    git-svn-id: http://piwigo.org/svn/extensions/ContactForm@19093 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/pt_BR/plugin.lang.php
@@ -0,0 +1,36 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Subject'] = 'Assunto';
+$lang['Tip'] = 'Dica';
+$lang['+ Add a category'] = '+ Adicionar categoria';
+$lang['+ Add an email'] = '+ Adicionar email';
+$lang['Active'] = 'Ativo';
+$lang['Add link in menu'] = 'Adicionar link ao menu';
+$lang['Allow guests to see the form'] = 'Permitir que convidados vejam o formulário';
+$lang['Category'] = 'Categoria';
+$lang['Default e-mail subject'] = 'Assunto padrão de e-mail';
+$lang['E-mail address is mandatory'] = 'Endereço de email é obrigatório';
+$lang['E-mail configuration'] = 'Configuração de email';
+$lang['E-mail format :'] = 'Format do email:';
+$lang['E-mail sent successfully'] = 'Email enviado com sucesso';
+?>
