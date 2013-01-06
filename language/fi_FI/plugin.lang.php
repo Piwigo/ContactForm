9e9119a66e1298b48db8046715ebf4d491f60a3e
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-01-06 07:40:50 +0000

    [contactform] Add fi_FI, thanks to : Juhi24
    git-svn-id: http://piwigo.org/svn/extensions/ContactForm@19885 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/fi_FI/plugin.lang.php
@@ -0,0 +1,35 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['%s must not be less than %d characters long'] = '%s ei saa olla lyhyempi kuin %d merkkiä';
+$lang['%s must not be more than %d characters long'] = '%s ei saa olla yli %d merkkiä pitkä';
+$lang['Add link in menu'] = 'Lisää linkki valikkoon';
+$lang['Name is mandatory'] = 'Nimi on pakollinen';
+$lang['Error while sending e-mail'] = 'Virhe sähköpostia lähetettäessä';
+$lang['Your name'] = 'Nimesi';
+$lang['Tip'] = 'Vinkki';
+$lang['Your e-mail'] = 'Sähköpostiosoitteesi';
+$lang['Send copy to my email'] = 'Lähetä kopio sähköpostiini';
+$lang['Subject'] = 'Aihe';
+$lang['Send'] = 'Lähetä';
+$lang['Message'] = 'Viesti';
+?>
