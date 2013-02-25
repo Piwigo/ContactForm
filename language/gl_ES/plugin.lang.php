d50f9b44ddd64fac9e2b7e6a9141b41b8f6cc6b7
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-02-25 17:50:25 +0000

    [contactform] Add gl_ES, thanks to : damufo
    git-svn-id: http://piwigo.org/svn/extensions/ContactForm@21032 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/gl_ES/plugin.lang.php
@@ -0,0 +1,60 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Redirect after sending email (optional)'] = 'Redireccionar despois de enviar o correo (opcional)';
+$lang['Please choose a category'] = 'Escolle unha categoría';
+$lang['Please enter a message'] = 'Insire un mensaxe';
+$lang['Please enter a name'] = 'Insire un nome';
+$lang['Please enter a subject'] = 'Insire un asunto';
+$lang['Please enter an e-mail'] = 'Insire un correo electrónico';
+$lang['Prefix of the sent e-mail subject'] = 'Prefixo do asunto da mensaxe a enviar';
+$lang['Send'] = 'Enviar';
+$lang['Send copy to my email'] = 'Enviar unha copia ó correo';
+$lang['Subject'] = 'Asunto';
+$lang['Text after the contact form'] = 'Texto despois do formulario de contacto';
+$lang['Text before the contact form'] = 'Texto antes do formulario de contacto';
+$lang['Tip'] = 'Truco';
+$lang['Your e-mail'] = 'Teu correo electrónico';
+$lang['Your name'] = 'Teu nome';
+$lang['can be translated with LocalFiles Editor plugin'] = 'pode ser traducido co engadido de modificación de ficheiros locais';
+$lang['you can use "%gallery_title%"'] = 'podes usar "%gallery_title%"';
+$lang['Active'] = 'Activo';
+$lang['No active email address'] = 'Non hai un enderezo de correo electrónico activo';
+$lang['%s must not be less than %d characters long'] = '%s non pode ter menos de %d caracteres de lonxitude';
+$lang['%s must not be more than %d characters long'] = '%d non pode ter máis de %s caracteres de lonxitude';
+$lang['+ Add a category'] = '+ Engadir categoría';
+$lang['+ Add an email'] = '+ Engadir correo electrónico';
+$lang['Add link in menu'] = 'Engadir ligazón ó menú';
+$lang['Allow guests to see the form'] = 'Permitir ós invitados ver o menú';
+$lang['Category'] = 'Categoría';
+$lang['Default e-mail subject'] = 'Asunto predeterminado para o correo electrónico';
+$lang['E-mail address is mandatory'] = 'O correo electrónico é obrigatorio';
+$lang['E-mail configuration'] = 'Configuración do correo electrónico';
+$lang['E-mail format :'] = 'Formato do correo electrónico:';
+$lang['E-mail sent successfully'] = 'O correo electrónico enviouse con éxito';
+$lang['Each category is displayed as a distinct "service" on the contact form (example: "Technical", "Commercial", "General question"). Using categories is not mandatory.'] = 'Cada categoría é amosada como un "servizo" distinto no formulario de contacto (exemplo: "Tecnico", "Comercial", "Preguntas xerais"). Non é obrigatorio o uso de categorías.';
+$lang['Error while sending e-mail'] = 'Produciuse un erro ó enviar o correo electrónico';
+$lang['General configuration'] = 'Información xeral';
+$lang['Message'] = 'Mensaxe';
+$lang['Name is mandatory'] = 'O nome é obrigatorio';
+$lang['Plain text'] = 'Texto simple';
+?>
