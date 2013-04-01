7cb124ded6b0d23f2fcd4bcb22e5aa67c2a6824b
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-04-01 07:21:04 +0000

    [contactform] Add nn_NO, thanks to : kiho
    git-svn-id: http://piwigo.org/svn/extensions/ContactForm@21936 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/nn_NO/plugin.lang.php
@@ -0,0 +1,60 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Please choose a category'] = 'Velg ein kategori';
+$lang['Please enter a message'] = 'Skriv inn melding';
+$lang['Please enter a name'] = 'Skriv inn namn';
+$lang['Please enter a subject'] = 'Skriv inn eit emne';
+$lang['Please enter an e-mail'] = 'Skriv inn ei e-postadresse';
+$lang['Prefix of the sent e-mail subject'] = 'Prefiks av det sendte e-postemnet';
+$lang['Redirect after sending email (optional)'] = 'Vidaresend etter sending av e-post (valfritt)';
+$lang['Send'] = 'Send';
+$lang['Send copy to my email'] = 'Send kopi til mi e-postadresse';
+$lang['Subject'] = 'Emne';
+$lang['Text after the contact form'] = 'Tekst på slutten av kontaktskjema';
+$lang['Text before the contact form'] = 'Tekst før kontaktskjema';
+$lang['Tip'] = 'Tips';
+$lang['Your e-mail'] = 'Di e-postadresse';
+$lang['Your name'] = 'Ditt namn';
+$lang['can be translated with LocalFiles Editor plugin'] = 'kan bli omsett med LocalFiles Editor tilleggsprogrammet';
+$lang['you can use "%gallery_title%"'] = 'du kan bruke "%gallery_title%"';
+$lang['%s must not be less than %d characters long'] = '%s må ikkje vera færre enn %d tegn lang';
+$lang['%s must not be more than %d characters long'] = '%s må ikkje vera meir enn %d tegn lang';
+$lang['+ Add a category'] = '+ Legg til kategori';
+$lang['+ Add an email'] = '+ Legg til e-post';
+$lang['Active'] = 'Aktiv';
+$lang['Add link in menu'] = 'Legg lenke til meny';
+$lang['Allow guests to see the form'] = 'Gje gjester lov til å sjå skjemaet';
+$lang['Category'] = 'Kategori';
+$lang['Default e-mail subject'] = 'Standard e-postemne';
+$lang['E-mail address is mandatory'] = 'E-postadresse er påkrevd';
+$lang['E-mail configuration'] = 'E-postkonfigurasjon';
+$lang['E-mail format :'] = 'E-postformat:';
+$lang['E-mail sent successfully'] = 'E-post er sendt';
+$lang['Each category is displayed as a distinct "service" on the contact form (example: "Technical", "Commercial", "General question"). Using categories is not mandatory.'] = 'Kvar kategori blir vist som ein distinkt "teneste" på kontaktskjema (døme: "Teknisk", "Kommersiell", "Generelt spørsmål"). Bruk av kategoriar er ikkje påkrevd.';
+$lang['Error while sending e-mail'] = 'Feil ved sending av e-post';
+$lang['General configuration'] = 'Generell konfigurasjon';
+$lang['Message'] = 'Melding';
+$lang['Name is mandatory'] = 'Namn er påkrevd';
+$lang['No active email address'] = 'Ingen aktiv e-postadresse';
+$lang['Plain text'] = 'Rein tekst';
+?>
