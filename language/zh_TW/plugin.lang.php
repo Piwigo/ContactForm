cbf327ce2299a45b6b3b300c2985ccdbdef86e9d
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-04-03 06:30:00 +0000

    [contactform] Add zh_TW, thanks to : tedx
    git-svn-id: http://piwigo.org/svn/extensions/ContactForm@21989 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/zh_TW/plugin.lang.php
@@ -0,0 +1,60 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Please choose a category'] = '請選擇類別';
+$lang['Please enter a message'] = '請輸入信息';
+$lang['Please enter a name'] = '請輸入姓名';
+$lang['Please enter a subject'] = '請輸入主旨';
+$lang['Please enter an e-mail'] = '請輸入電子郵件';
+$lang['Prefix of the sent e-mail subject'] = '已發送的電子郵件主旨字首';
+$lang['Redirect after sending email (optional)'] = '發送電子郵件之後重新導向（可選）';
+$lang['Send'] = '發送';
+$lang['Send copy to my email'] = '將副本發送到我的郵箱';
+$lang['Subject'] = '主旨';
+$lang['Text after the contact form'] = '聯絡表格之後的文字';
+$lang['Text before the contact form'] = '聯絡表格之前的文字';
+$lang['Tip'] = '提示';
+$lang['Your e-mail'] = '您的電子信箱';
+$lang['Your name'] = '您的姓名';
+$lang['can be translated with LocalFiles Editor plugin'] = '可以被翻譯在LocalFiles編輯器插件內';
+$lang['you can use "%gallery_title%"'] = '您可以使用 "%gallery_title%"';
+$lang['Each category is displayed as a distinct "service" on the contact form (example: "Technical", "Commercial", "General question"). Using categories is not mandatory.'] = '每個類別將顯示為一個獨特的“服務”上的聯繫方式(例如：“技術”，“商業”，“一般問題”)。通過分類，兒不是強制性的。';
+$lang['Error while sending e-mail'] = '發送電子郵件時發生錯誤';
+$lang['General configuration'] = '一般設定';
+$lang['Message'] = '信息';
+$lang['Name is mandatory'] = '名稱是必需的';
+$lang['No active email address'] = '沒有有效的電子郵件地址';
+$lang['Plain text'] = '純文字';
+$lang['%s must not be less than %d characters long'] = '%s 一定不能低於 %d 個字符長度';
+$lang['%s must not be more than %d characters long'] = '%s 一定不能超過 %d 個字符長度';
+$lang['+ Add a category'] = '+ 新增類別';
+$lang['+ Add an email'] = '+ 新增電子郵件';
+$lang['Active'] = '起動';
+$lang['Add link in menu'] = '選單中新增連結';
+$lang['Allow guests to see the form'] = '允許訪客看表格';
+$lang['Category'] = '類別';
+$lang['Default e-mail subject'] = '預設電子郵件的主旨';
+$lang['E-mail address is mandatory'] = '電子郵件地址是必需的';
+$lang['E-mail configuration'] = '電子郵件設定';
+$lang['E-mail format :'] = '電子郵件格式：';
+$lang['E-mail sent successfully'] = '發送的電子郵件成功';
+?>
