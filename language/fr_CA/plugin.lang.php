9adc1106c9d3e5462f96a43cf49eede0a9a2c08f
ddtddt <ddtddt@piwigo.org>
2013-04-24 06:21:10 +0000

    [extensions] - ContactForm - update fr_CA - Quebec - Québécois - Thanks to TiOger
    git-svn-id: http://piwigo.org/svn/extensions/ContactForm@22350 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/fr_CA/plugin.lang.php
@@ -0,0 +1,63 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+
+$lang['Your name'] = 'Votre nom';
+$lang['Your e-mail'] = 'Adresse de courriel';
+$lang['Subject'] = 'Sujet';
+$lang['Message'] = 'Message';
+$lang['Category'] = 'Catégorie';
+$lang['General configuration'] = 'Configuration générale';
+$lang['Add link in menu'] = 'Ajouter le lien dans le menu';
+$lang['Allow guests to see the form'] = 'Autoriser les invités à avoir le formulaire';
+$lang['E-mail address is mandatory'] = 'Adresse de courriel obligatoire';
+$lang['Name is mandatory'] = 'Présence du nom obligatoire';
+$lang['E-mail configuration'] = 'Configuration des courriels';
+$lang['E-mail format :'] = 'Format des courriels :';
+$lang['Plain text'] = 'Texte brut';
+$lang['Default e-mail subject'] = 'Sujet par défaut';
+$lang['can be translated with LocalFiles Editor plugin'] = 'peut être traduit avec le greffon LocalFiles Editor';
+$lang['Prefix of the sent e-mail subject'] = 'Préfixe du sujet du courriel envoyé';
+$lang['you can use "%gallery_title%"'] = 'vous pouvez utiliser "%gallery_title%"';
+$lang['Text before the contact form'] = 'Texte au-dessus du formulaire de contact';
+$lang['Text after the contact form'] = 'Texte au-dessous du formulaire de contact';
+$lang['E-mails'] = 'Courriels';
+$lang['Active'] = 'Actif';
+$lang['+ Add an email'] = '+ Ajouter un courriel';
+$lang['+ Add a category'] = '+ Ajouter une categorie';
+$lang['No active email address'] = 'Aucune adresse de courriel active';
+$lang['E-mail sent successfully'] = 'Courriel envoyé avec succès';
+$lang['Please enter a name'] = 'Veuillez entrer un nom';
+$lang['Please enter an e-mail'] = 'Veuillez entrer un courriel';
+$lang['Please enter a subject'] = 'Veuillez entrer un sujet';
+$lang['Please enter a message'] = 'Veuillez entrer un message';
+$lang['Please choose a category'] = 'Veuillez choisir une categorie';
+$lang['%s must not be less than %d characters long'] = '%s ne doit pas faire moins de %d caractères';
+$lang['%s must not be more than %d characters long'] = '%s ne doit pas faire plus de %d caractères';
+$lang['Error while sending e-mail'] = 'Erreur lors de l\'envoi du courriel';
+$lang['Redirect after sending email (optional)'] = 'Rediriger après l\'envoi du courriel (optionel)';
+$lang['Tip'] = 'Astuce';
+$lang['Each category is displayed as a distinct "service" on the contact form (example: "Technical", "Commercial", "General question"). Using categories is not mandatory.'] = 'Chaque catégorie est affichée comme un "service" distinct sur la page de contact (exemple: "Technique", "Commercial", "Question générale"). Utiliser les catégories n\'est pas obligatoire.';
+$lang['Send'] = 'Envoyer';
+$lang['Send copy to my email'] = 'Envoyer une copie à mon adresse de courriel';
+
+?>
