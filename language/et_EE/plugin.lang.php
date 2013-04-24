a6a721c2cd8cc02630e5364ba6d6ccd667714cd5
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-04-24 05:31:34 +0000

    [contactform] Add et_EE, thanks to : Eggy
    git-svn-id: http://piwigo.org/svn/extensions/ContactForm@22348 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/et_EE/plugin.lang.php
@@ -0,0 +1,60 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Prefix of the sent e-mail subject'] = 'Saadetud e-maili teema prefiks';
+$lang['Redirect after sending email (optional)'] = 'Suuna ümber pärast saatmist (valikuline)';
+$lang['Send'] = 'Saada';
+$lang['Send copy to my email'] = 'Saada koopia minu e-mailile';
+$lang['Subject'] = 'Teema';
+$lang['Text after the contact form'] = 'Tekst pärast kontakteerumis vormi';
+$lang['Text before the contact form'] = 'Teks enne kontakteerumise vormi';
+$lang['Tip'] = 'Nõuanne';
+$lang['Your e-mail'] = 'Sinua e-mail';
+$lang['Your name'] = 'Sinu nimi';
+$lang['can be translated with LocalFiles Editor plugin'] = 'saab tõlkida LocalFiles Editor lisa abil';
+$lang['you can use "%gallery_title%"'] = 'saab kasutada "%gallery_title%"';
+$lang['E-mail sent successfully'] = 'E-kiri edukalt saadetud';
+$lang['Each category is displayed as a distinct "service" on the contact form (example: "Technical", "Commercial", "General question"). Using categories is not mandatory.'] = 'Iga kategooria on kui eraldi punkt kontakti vormil(näiteks "tehniline", "üldine küsimus" jne), nende kasutamine pole kohustuslik.';
+$lang['Error while sending e-mail'] = 'Viga kirja saatmisel';
+$lang['General configuration'] = 'Üldine seadistus';
+$lang['Message'] = 'Teade';
+$lang['Name is mandatory'] = 'Nimi on kohustulik';
+$lang['No active email address'] = 'Aktiivne e-maili aadress puudub';
+$lang['Plain text'] = 'Ainult tekst';
+$lang['Please choose a category'] = 'Palun vali kategooria';
+$lang['Please enter a message'] = 'Palun sisesta sõnum';
+$lang['Please enter a name'] = 'Palun sisesta nimi';
+$lang['Please enter a subject'] = 'Palun sisesta teema';
+$lang['Please enter an e-mail'] = 'Palun sisesta e-maili aadress';
+$lang['%s must not be less than %d characters long'] = '%s peab olema lühem kui %d tähemärki';
+$lang['%s must not be more than %d characters long'] = '%s ei tohi olla pikem kui %d tähemärki';
+$lang['+ Add a category'] = '+ Lisa kategooria';
+$lang['+ Add an email'] = '+ Lisa e-kiri';
+$lang['Active'] = 'Aktiivne';
+$lang['Add link in menu'] = 'Lisa menüüsse link';
+$lang['Allow guests to see the form'] = 'Luba külalistel vormi näha';
+$lang['Category'] = 'Kategooria';
+$lang['Default e-mail subject'] = 'Vaikimisi e-kirja teema';
+$lang['E-mail address is mandatory'] = 'E-posti aadress on kohustuslik';
+$lang['E-mail configuration'] = 'E-kirja kinnitus';
+$lang['E-mail format :'] = 'E-kirja formaat';
+?>
