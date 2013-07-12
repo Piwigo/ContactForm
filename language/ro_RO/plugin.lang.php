521110d160abcd5bc2c7f5dc885b67f0cbd8b74f
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-07-12 15:23:35 +0000

    [contactform] Update ro_RO, thanks to : smarmie
    git-svn-id: http://piwigo.org/svn/extensions/ContactForm@23931 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/ro_RO/plugin.lang.php
@@ -0,0 +1,60 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Each category is displayed as a distinct "service" on the contact form (example: "Technical", "Commercial", "General question"). Using categories is not mandatory.'] = 'Fiecare categorie este afișată ca un "serviciu" distinct în formularul de contact (exemplu: "Tehnic", "Comercial", "Întrebari generale"). Folosirea categoriilor nu este obligatorie.';
+$lang['Redirect after sending email (optional)'] = 'Redirecționează după trimiterea e-mail-ului (opțional)';
+$lang['Text after the contact form'] = 'Text după formularul de contact';
+$lang['Text before the contact form'] = 'Text înainte de formularul de contact';
+$lang['Tip'] = 'Pont';
+$lang['Please choose a category'] = 'Alege o categorie';
+$lang['Please enter a message'] = 'Introdu un mesaj';
+$lang['Please enter a name'] = 'Introdu un nume';
+$lang['Please enter a subject'] = 'Introdu un subiect';
+$lang['Please enter an e-mail'] = 'Introdu un e-mail';
+$lang['Prefix of the sent e-mail subject'] = 'Prefix-ul subiectului e-mail-ului trimis';
+$lang['Send'] = 'Trimite';
+$lang['Send copy to my email'] = 'Trimite o copie la e-mail-ul meu';
+$lang['Subject'] = 'Subiect';
+$lang['Your e-mail'] = 'E-mail-ul tău';
+$lang['Your name'] = 'Numele tău';
+$lang['can be translated with LocalFiles Editor plugin'] = 'poate fi tradus cu plugin-ul Editare Fișiere Locale';
+$lang['you can use "%gallery_title%"'] = 'poți folosi "%gallery_title%"';
+$lang['%s must not be less than %d characters long'] = '%s nu trebuie să fie mai scurt de %d caractere';
+$lang['%s must not be more than %d characters long'] = '%s nu trebuie să fie mai lung de %d caractere';
+$lang['+ Add a category'] = '+ Adaugă o categorie';
+$lang['+ Add an email'] = '+ Adaugă o adresă de e-mail';
+$lang['Active'] = 'Activ';
+$lang['Add link in menu'] = 'Adaugă legătură în meniu';
+$lang['Allow guests to see the form'] = 'Permite oaspeților să vadă formularul';
+$lang['Category'] = 'Categorie';
+$lang['Default e-mail subject'] = 'Subiectul implicit pentru e-mail';
+$lang['E-mail address is mandatory'] = 'Adresa de e-mail este obligatorie';
+$lang['E-mail configuration'] = 'Configurație e-maile';
+$lang['E-mail format :'] = 'Formatare e-mailȘ';
+$lang['E-mail sent successfully'] = 'E-mail trimis cu succes';
+$lang['Error while sending e-mail'] = 'Eroare la trimiterea e-mail-ului';
+$lang['General configuration'] = 'Configurație generală';
+$lang['Message'] = 'Mesaj';
+$lang['Name is mandatory'] = 'Numele este obligatoriu';
+$lang['No active email address'] = 'Nicio adresă de e-mail activă';
+$lang['Plain text'] = 'Text simplu';
+?>
