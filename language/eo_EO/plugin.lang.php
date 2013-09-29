ba06ffa3c4a654363ca15de0c2e39fe7ce1e232b
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-09-29 20:01:24 +0000

    [contactform] Add eo_EO, thanks to : sylvbarrier
    git-svn-id: http://piwigo.org/svn/extensions/ContactForm@24689 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/eo_EO/plugin.lang.php
@@ -0,0 +1,60 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Please choose a category'] = 'Bonvolu elekti kategorion';
+$lang['Please enter a message'] = 'Bonvolu redakti mesaĝon';
+$lang['Please enter a name'] = 'Bonvolu redakti nomon';
+$lang['Please enter a subject'] = 'Bonvolu redakti temon';
+$lang['Please enter an e-mail'] = 'Bonvolu redakti retadreson';
+$lang['Prefix of the sent e-mail subject'] = 'Prefikso de la temo de la sendita retmesaĝo';
+$lang['Redirect after sending email (optional)'] = 'Alidirekti post la sendo de la retmesaĝo (opcia)';
+$lang['Send'] = 'Sendi';
+$lang['Send copy to my email'] = 'Sendi kopion al mia retadreso';
+$lang['Subject'] = 'Temo';
+$lang['Text after the contact form'] = 'Teksto post la kontakta formularo';
+$lang['Text before the contact form'] = 'Teksto antaŭ la kontakta formularo';
+$lang['Tip'] = 'Truko';
+$lang['Your e-mail'] = 'Via retadreso';
+$lang['Your name'] = 'Via nomo';
+$lang['can be translated with LocalFiles Editor plugin'] = 'povas esti tradukita kun la kromaĵo "LocalFiles Editor" (Redaktilo de lokaj dosieroj)';
+$lang['you can use "%gallery_title%"'] = 'eblas al vi uzi "%gallery_title%"';
+$lang['%s must not be less than %d characters long'] = 'nepras, ke %s ne longu pli ol %d signoj';
+$lang['%s must not be more than %d characters long'] = 'nepras, ke %s ne longu malpli ol %d signoj';
+$lang['+ Add a category'] = '+ Aldoni kategorion';
+$lang['+ Add an email'] = '+ Aldoni retmesaĝon';
+$lang['Active'] = 'Aktiva';
+$lang['Add link in menu'] = 'Aldoni ligilon en menuon';
+$lang['Allow guests to see the form'] = 'Ebligi al vizitantoj vidi la formularon';
+$lang['Category'] = 'Kategorio';
+$lang['Default e-mail subject'] = 'Apriora retmesaĝa temo';
+$lang['E-mail address is mandatory'] = 'Retadreso devigas';
+$lang['E-mail configuration'] = 'Retmesaĝa agordo';
+$lang['E-mail format :'] = 'Retmesaĝa formato:';
+$lang['E-mail sent successfully'] = 'Retmesaĝo sukcese sendiĝis';
+$lang['Each category is displayed as a distinct "service" on the contact form (example: "Technical", "Commercial", "General question"). Using categories is not mandatory.'] = 'Ĉiu kategorio montriĝas kiel aparta "servo" ĉe la kontaktoformularo (ekz-e: "Teknika", "Komerca", "Ĝenerala demando"). Uzi kategoriojn ne devigas.';
+$lang['Error while sending e-mail'] = 'Eraro dum retmesaĝosendo';
+$lang['General configuration'] = 'Ĝenerala agordo';
+$lang['Message'] = 'Mesaĝo';
+$lang['Name is mandatory'] = 'Nomo devigas';
+$lang['No active email address'] = 'Retadreso ne aktivas';
+$lang['Plain text'] = 'Plata teksto';
+?>
