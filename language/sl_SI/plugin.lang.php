8eadf14226eac955973581011ce4f1113588b876
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-12-29 13:18:50 +0000

    [contactform] Update sl_SI, thanks to : simon.krajnc
    git-svn-id: http://piwigo.org/svn/extensions/ContactForm@26311 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/sl_SI/plugin.lang.php
@@ -0,0 +1,62 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Name is mandatory'] = 'Obvezno ime';
+$lang['No active email address'] = 'Ni aktivnih e-poštnih naslovov';
+$lang['Plain text'] = 'Tekst';
+$lang['Please choose a category'] = 'Prosim izberi kategorijo';
+$lang['Please enter a message'] = 'Prosim vnesi sporočilo';
+$lang['Please enter a name'] = 'Prosim vnesi ime';
+$lang['Please enter a subject'] = 'Prosim vnesi naslov';
+$lang['Please enter an e-mail'] = 'Prosim vnesi e-pošto';
+$lang['Prefix of the sent e-mail subject'] = 'Prefix poslanega e-poštnega naslova';
+$lang['Redirect after sending email (optional)'] = 'Preusmeri odposlano e-pošto (izbirno)';
+$lang['Send'] = 'Pošlji';
+$lang['Send copy to my email'] = 'Pošlji kopijo poslane e-pošte na moj naslov';
+$lang['Subject'] = 'Naslov';
+$lang['Text after the contact form'] = 'Besedilo za kontaktnim poljem';
+$lang['Text before the contact form'] = 'Besedilo pred kontaktnim poljem';
+$lang['Tip'] = 'Nasvet';
+$lang['Your e-mail'] = 'Tvoja e-pošta';
+$lang['Your name'] = 'Tvoje ime';
+$lang['can be translated with LocalFiles Editor plugin'] = 'lahko je prevedeno z LocalFiles Editor vtičnikom';
+$lang['you can use "%gallery_title%"'] = 'lahko uporabiš "%naslov_galerije%"';
+$lang['%s must not be less than %d characters long'] = '%s ne sme biti krajše od %d znakov';
+$lang['%s must not be more than %d characters long'] = '%s ne sme biti daljše %d znakov';
+$lang['+ Add a category'] = 'Dodaj kategorijo';
+$lang['+ Add an email'] = 'Dodaj e-pošto';
+$lang['Active'] = 'Aktivni';
+$lang['Add link in menu'] = 'Dodaj povezavo v meni';
+$lang['Allow guests to see the form'] = 'Dovoli gostom vidnost forme';
+$lang['Category'] = 'Kategorija';
+$lang['Default e-mail subject'] = 'Prednastavljeni e-poštni naslov';
+$lang['E-mail address is mandatory'] = 'Obvezen naslov v E-pošti';
+$lang['E-mail configuration'] = 'Nastavitev E-pošte';
+$lang['E-mail format :'] = 'Oblika e-pošte:';
+$lang['E-mail sent successfully'] = 'E-pošta je uspešno poslana';
+$lang['Each category is displayed as a distinct "service" on the contact form (example: "Technical", "Commercial", "General question"). Using categories is not mandatory.'] = 'Vsaka kategorija je prikazana kot ustrezna "storitev" na kontaktni formi  (primer: "Tehnično", "Poslovno", "Splošno vprašanje"). Uporaba kategorij ni obvezujoča.';
+$lang['Error while sending e-mail'] = 'Napaka med pošiljanjem e-pošte';
+$lang['General configuration'] = 'Glavna konfiguracija';
+$lang['Invalid redirect URL'] = 'Napačna URL preusmeritev';
+$lang['Message'] = 'Sporočilo';
+$lang['Name'] = 'Ime';
+?>
