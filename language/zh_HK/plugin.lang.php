0d12a1985aecb54403281f4ef832b5f98b12ee4e
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-01-16 08:45:11 +0000

    [contactform] Update zh_HK, thanks to : hkdigit
    git-svn-id: http://piwigo.org/svn/extensions/ContactForm@26792 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/zh_HK/plugin.lang.php
@@ -0,0 +1,62 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2014 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Each category is displayed as a distinct "service" on the contact form (example: "Technical", "Commercial", "General question"). Using categories is not mandatory.'] = '聯繫表格類別顯示為"服務"（例如："技術"，"商業"，"常見問題"）。使用的類別不是強制性的。';
+$lang['No active email address'] = '沒有有效的電郵地址';
+$lang['Plain text'] = '純文本';
+$lang['Please choose a category'] = '請選擇類別';
+$lang['Please enter a message'] = '請輸入信息';
+$lang['Please enter a name'] = '請輸入姓名';
+$lang['Please enter a subject'] = '請輸入主題';
+$lang['Please enter an e-mail'] = '請輸入您的電子郵件';
+$lang['Prefix of the sent e-mail subject'] = '發送電子郵件的主題前綴';
+$lang['Redirect after sending email (optional)'] = '發送電子郵件後重新導向（可選）';
+$lang['Send'] = '發送';
+$lang['Send copy to my email'] = '電郵副本給我';
+$lang['Subject'] = '主題';
+$lang['Text after the contact form'] = '聯繫表格後文';
+$lang['Text before the contact form'] = '聯繫表格前文';
+$lang['Tip'] = '提示';
+$lang['Your e-mail'] = '您的電子郵件';
+$lang['Your name'] = '你的名字';
+$lang['can be translated with LocalFiles Editor plugin'] = '可以用LocalFiles編輯器插件翻譯';
+$lang['you can use "%gallery_title%"'] = '您可以使用 "%gallery_title%"';
+$lang['%s must not be less than %d characters long'] = '%s必須不小於長%d個字符';
+$lang['%s must not be more than %d characters long'] = '%s必須不能超過長%d個字符';
+$lang['+ Add a category'] = '+添加類別';
+$lang['+ Add an email'] = '+添加電子郵件';
+$lang['Active'] = '有效';
+$lang['Add link in menu'] = '在功能表添加連結';
+$lang['Allow guests to see the form'] = '讓訪客到表單';
+$lang['Category'] = '類別';
+$lang['Default e-mail subject'] = '默認電子郵件主題';
+$lang['E-mail address is mandatory'] = '電子郵件地址是必須的';
+$lang['E-mail configuration'] = '電子郵件配置';
+$lang['E-mail format :'] = '電子郵件格式：';
+$lang['E-mail sent successfully'] = '電子郵件成功發送';
+$lang['Error while sending e-mail'] = '在發送郵件出現錯誤';
+$lang['General configuration'] = '一般配置';
+$lang['Invalid redirect URL'] = '無效的重新導向鏈接';
+$lang['Message'] = '信息';
+$lang['Name'] = '名稱';
+$lang['Name is mandatory'] = '名字是必須的';
+?>
