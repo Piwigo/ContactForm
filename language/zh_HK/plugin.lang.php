b93551c743d947b86f9ef5cbabb7bcf08c0440e7
Piwigo-TranslationTeam <translate-github@piwigo.org>
2016-03-10 22:51:44 +0100

    Update zh_HK, thanks to hkdigit

--- language/zh_HK/plugin.lang.php
+++ language/zh_HK/plugin.lang.php
@@ -20,10 +20,8 @@
 // | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
 // | USA.                                                                  |
 // +-----------------------------------------------------------------------+
-$lang['Each category is displayed as a distinct "service" on the contact form (example: "Technical", "Commercial", "General question"). Using categories is not mandatory.'] = '聯繫表格類別顯示為"服務"（例如："技術"，"商業"，"常見問題"）。使用的類別不是強制性的。';
 $lang['No active email address'] = '沒有有效的電郵地址';
 $lang['Plain text'] = '純文本';
-$lang['Please choose a category'] = '請選擇類別';
 $lang['Please enter a message'] = '請輸入信息';
 $lang['Please enter a name'] = '請輸入姓名';
 $lang['Please enter a subject'] = '請輸入主題';
@@ -35,19 +33,14 @@ $lang['Send copy to my email'] = '電郵副本給我';
 $lang['Subject'] = '主題';
 $lang['Text after the contact form'] = '聯繫表格後文';
 $lang['Text before the contact form'] = '聯繫表格前文';
-$lang['Tip'] = '提示';
 $lang['Your e-mail'] = '您的電子郵件';
 $lang['Your name'] = '你的名字';
 $lang['can be translated with LocalFiles Editor plugin'] = '可以用LocalFiles編輯器插件翻譯';
 $lang['you can use "%gallery_title%"'] = '您可以使用 "%gallery_title%"';
 $lang['%s must not be less than %d characters long'] = '%s必須不小於長%d個字符';
 $lang['%s must not be more than %d characters long'] = '%s必須不能超過長%d個字符';
-$lang['+ Add a category'] = '+添加類別';
-$lang['+ Add an email'] = '+添加電子郵件';
-$lang['Active'] = '有效';
 $lang['Add link in menu'] = '在功能表添加連結';
 $lang['Allow guests to see the form'] = '讓訪客到表單';
-$lang['Category'] = '類別';
 $lang['Default e-mail subject'] = '默認電子郵件主題';
 $lang['E-mail address is mandatory'] = '電子郵件地址是必須的';
 $lang['E-mail configuration'] = '電子郵件配置';
@@ -59,4 +52,10 @@ $lang['Invalid redirect URL'] = '無效的重新導向鏈接';
 $lang['Message'] = '信息';
 $lang['Name'] = '名稱';
 $lang['Name is mandatory'] = '名字是必須的';
-?>
+$lang['Additional emails'] = '其他電郵';
+$lang['E-mail already added'] = '電子郵件已添加';
+$lang['Manage contact e-mails'] = '管理聯繫人電子郵件';
+$lang['New e-mail'] = '新電郵';
+$lang['Select a new user'] = '選擇新用戶';
+$lang['This user has no e-mail'] = '該用戶沒有電子郵件';
+$lang['User already added'] = '用戶已添加';
