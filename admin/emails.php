a5f074c77e0208982e75c3ea841b11a9b9506379
plegall <plg@piwigo.org>
2017-06-01 16:20:25 +0200

    remove form submission code, all is managed with Ajax calls on API

--- admin/emails.php
+++ admin/emails.php
@@ -1,46 +1,6 @@
 <?php
 if (!defined('CONTACT_FORM_PATH')) die('Hacking attempt!');
 
-// save emails
-if (isset($_POST['save_emails']))
-{
-  $emails = array();
-  foreach ($_POST['emails'] as $entry)
-  {
-    if (isset($entry['delete'])) continue;
-
-    if ( empty($entry['email']) or !email_check_format($entry['email']) )
-    {
-      $page['errors'][] = l10n('mail address must be like xxx@yyy.eee (example : jack@altern.org)');
-    }
-    else
-    {
-      if (empty($entry['name'])) $entry['name'] = $entry['email'];
-      if ($entry['group_name'] == -1) $entry['group_name'] = null;
-
-      $emails[] = array(
-        'name' => $entry['name'],
-        'email' => $entry['email'],
-        'group_name' => $entry['group_name'],
-        'active' => boolean_to_string(isset($entry['active'])),
-        );
-    }
-  }
-
-  pwg_query('TRUNCATE TABLE `'. CONTACT_FORM_TABLE. '`;');
-
-  mass_inserts(
-    CONTACT_FORM_TABLE,
-    array('name','email','group_name','active'),
-    $emails
-    );
-
-  $conf['ContactForm_ready'] = count($emails);
-
-  $page['infos'][] = l10n('Information data registered in database');
-}
-
-
 // display emails
 $query = '
 SELECT cf.id,
