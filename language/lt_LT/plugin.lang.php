2cdf9c3686eb2db96259e5dc15410f8680188665
Piwigo-TranslationTeam <translate-github@piwigo.org>
2017-10-15 07:06:17 +0200

    Add lt_LT, thanks to vilisas

---
+++ language/lt_LT/plugin.lang.php
@@ -0,0 +1,61 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2017 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Additional emails'] = 'Papildomi el. pašto adresai';
+$lang['New e-mail'] = 'Naujas el. paštas';
+$lang['No active email address'] = 'Nėra aktyvių el. pašto adresų';
+$lang['E-mail address is mandatory'] = 'El. pašto laukelis privalomas';
+$lang['E-mail already added'] = 'El. paštas jau pridėtas';
+$lang['E-mail configuration'] = 'El. pašto konfigūracija';
+$lang['E-mail format :'] = 'El. pašto formatas:';
+$lang['Manage contact e-mails'] = 'Redaguoti kontaktinius el. pašto adresus';
+$lang['Plain text'] = 'Tik tekstas';
+$lang['Please enter a message'] = 'Įveskite pranešimo tekstą';
+$lang['Please enter a name'] = 'Įveskite vardą';
+$lang['Please enter a subject'] = 'Įveskite temą';
+$lang['Please enter an e-mail'] = 'Įveskite el. pašto adresą';
+$lang['Prefix of the sent e-mail subject'] = 'Pranešimo temos prefiksas';
+$lang['Redirect after sending email (optional)'] = 'Išsiuntus pranešimą nukreipti (nebūtina)';
+$lang['Select a new user'] = 'Pasirinkite naują lankytoją';
+$lang['Send'] = 'Siųsti';
+$lang['Send copy to my email'] = 'Siųsti kopiją į savo el. paštą';
+$lang['Subject'] = 'Tema';
+$lang['Text after the contact form'] = 'Tekstas po kontaktine forma';
+$lang['Text before the contact form'] = 'Tekstas kontaktinės formos pradžioje';
+$lang['This user has no e-mail'] = 'Šis lankytojas neturi el. pašto';
+$lang['User already added'] = 'Lankytojas jau pridėtas';
+$lang['Your e-mail'] = 'Jūsų el. paštas';
+$lang['Your name'] = 'Jūsų vardas';
+$lang['can be translated with LocalFiles Editor plugin'] = 'gali būti išversta su LocalFiles Editor priedu ';
+$lang['you can use "%gallery_title%"'] = 'galite naudoti "%gallery_title%" kintamąjį';
+$lang['%s must not be more than %d characters long'] = '%s turi būti ne ilgesnis, nei %d simbolių';
+$lang['Add link in menu'] = 'Pridėti į meniu';
+$lang['Allow guests to see the form'] = 'Leisti svečiams matyti formą';
+$lang['Default e-mail subject'] = 'Tema pagal nutylėjimą';
+$lang['E-mail sent successfully'] = 'Pranešimas sėkmingai išsiųstas';
+$lang['Error while sending e-mail'] = 'Klaida siunčiant pranešimą';
+$lang['General configuration'] = 'Bendra konfigūracija';
+$lang['Invalid redirect URL'] = 'Neteisingas peradresavimo URL';
+$lang['Message'] = 'Pranešimas';
+$lang['Name'] = 'Vardas';
+$lang['Name is mandatory'] = 'Vardas yra privalomas';
+$lang['%s must not be less than %d characters long'] = '%s turi būti ne trumpesnis nei %d simbolių';
