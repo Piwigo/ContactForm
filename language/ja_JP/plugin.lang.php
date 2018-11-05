6d0a13c9ff2dd4811cd5ad0d5c35ab513359843e
Piwigo-TranslationTeam <translate-github@piwigo.org>
2018-11-05 20:59:42 +0100

    Add ja_JP, thanks to Trumpet

---
+++ language/ja_JP/plugin.lang.php
@@ -0,0 +1,61 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2017 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Plain text'] = 'プレーンテキスト';
+$lang['Please enter a message'] = 'メッセージを入力して下さい';
+$lang['Please enter a name'] = '名前を入力して下さい';
+$lang['Please enter a subject'] = '題目を入力して下さい';
+$lang['Please enter an e-mail'] = 'Eメールを入力して下さい';
+$lang['Prefix of the sent e-mail subject'] = '送信Eメール題目のプレフィクス';
+$lang['Redirect after sending email (optional)'] = 'Eメール送信後にリダイレクトする（オプショナル）';
+$lang['Select a new user'] = '新しいユーザを選択';
+$lang['Send'] = '送信する';
+$lang['Send copy to my email'] = 'コピーを自分のEメールに送信する';
+$lang['Subject'] = '題目';
+$lang['Text after the contact form'] = '連絡フォーム後の文字列';
+$lang['Text before the contact form'] = '連絡フォーム前の文字列';
+$lang['This user has no e-mail'] = 'このユーザはEメールを持っていません';
+$lang['User already added'] = 'ユーザは既に追加されています';
+$lang['Your e-mail'] = 'あなたのEメール';
+$lang['Your name'] = 'あなたの名前';
+$lang['can be translated with LocalFiles Editor plugin'] = 'LocalFiles Editorプラグインで翻訳される';
+$lang['you can use "%gallery_title%"'] = '変数 %gallery_title% を使うことができます';
+$lang['%s must not be less than %d characters long'] = '%s は %d 文字以上でなければなりません。';
+$lang['%s must not be more than %d characters long'] = '%s は %d 文字以下でなければなりません。';
+$lang['Add link in menu'] = 'メニューにリンクを追加';
+$lang['Additional emails'] = '追加のEメール';
+$lang['Allow guests to see the form'] = 'ゲストにこのフォームの閲覧を許可';
+$lang['Default e-mail subject'] = 'デフォルトのEメール題目';
+$lang['E-mail address is mandatory'] = 'Eメールアドレスは必須です';
+$lang['E-mail already added'] = 'Eメールは既に追加されています';
+$lang['E-mail configuration'] = 'Eメールの設定';
+$lang['E-mail format :'] = 'Eメールフォーマット：';
+$lang['E-mail sent successfully'] = 'Eメール送信に成功しました';
+$lang['Error while sending e-mail'] = 'Eメール送信中のエラー';
+$lang['General configuration'] = '一般設定';
+$lang['Invalid redirect URL'] = '不正なリダイレクトURL';
+$lang['Manage contact e-mails'] = '連絡先Eメールの管理';
+$lang['Message'] = 'メッセージ';
+$lang['Name'] = '名前';
+$lang['Name is mandatory'] = '名前は必須項目です';
+$lang['New e-mail'] = '新しいEメール';
+$lang['No active email address'] = '有効なEメールアドレスがありません';
