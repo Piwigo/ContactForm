a1c38c3d881f1f04c246b23a18753826e966c20f
Piwigo-TranslationTeam <translate-github@piwigo.org>
2021-07-07 11:53:26 +0200

    Update eu_ES, thanks to bixerdo

---
+++ language/eu_ES/plugin.lang.php
@@ -0,0 +1,46 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Plain text'] = 'Testu soila';
+$lang['Please enter a message'] = 'Idatzi mezuren bat';
+$lang['Please enter a name'] = 'Idatzi izenen bat';
+$lang['Please enter a subject'] = 'Idatzi gairen bat';
+$lang['Please enter an e-mail'] = 'Idatzi helbide elektronikoren bat';
+$lang['Prefix of the sent e-mail subject'] = 'Bidalitako mezu elektronikoaren gaiaren aurrizkia';
+$lang['Redirect after sending email (optional)'] = 'Birbideratu mezu elektronikoa bidali ostean (aukerazkoa)';
+$lang['Select a new user'] = 'Hautatu erabiltzaile berria';
+$lang['Send'] = 'Bidali';
+$lang['Send copy to my email'] = 'Bidali kopia nire helbide elektronikora';
+$lang['Subject'] = 'Gaia';
+$lang['Text after the contact form'] = 'Harremanetarako formularioaren osteko testua';
+$lang['Text before the contact form'] = 'Harremanetarako formularioaren aurreko testua';
+$lang['This user has no e-mail'] = 'Erabiltzaile honek ez du posta elektroniko helbiderik';
+$lang['User already added'] = 'Erabiltzailea dagoeneko badago';
+$lang['Your e-mail'] = 'Zure posta elektronikoa';
+$lang['Your name'] = 'Zure izena';
+$lang['can be translated with LocalFiles Editor plugin'] = 'LocalFiles Editor pluginarekin euskaratu daiteke';
+$lang['you can use "%gallery_title%"'] = '"%gallery_title" aldagaia erabil dezakezu';
+$lang['%s must not be less than %d characters long'] = '%s ezin da izan %d baino karaktere gutxiagokoa';
+$lang['%s must not be more than %d characters long'] = '%s ezin da izan %d baino karaktere gehiagokoa';
+$lang['Add link in menu'] = 'Gehitu esteka menuan';
+$lang['Additional emails'] = 'Helbide elektroniko gehigarriak';
+$lang['Allow guests to see the form'] = 'Baimendu gonbidatuei formularioa ikustea';
+$lang['Default e-mail subject'] = 'Lehenetsitako gaia mezurako';
+$lang['E-mail address is mandatory'] = 'Posta elektroniko helbidea derrigorrezkoa da';
+$lang['E-mail already added'] = 'Dagoeneko gehitutako posta elektroniko helbidea';
+$lang['E-mail configuration'] = 'Posta elektronikoaren hobespenak';
+$lang['E-mail format :'] = 'Posta elektronikoaren formatua:';
+$lang['E-mail sent successfully'] = 'Mezua zuzen bidali da';
+$lang['Error while sending e-mail'] = 'Errorea gertatu da mezua bidaltzerakoan';
+$lang['General configuration'] = 'Hobespen orokorrak';
+$lang['Invalid redirect URL'] = 'Baliogabeko birbidaratze URLa';
+$lang['Manage contact e-mails'] = 'Kudeatu kontakturako helbide elektronikoak';
+$lang['Message'] = 'Mezua';
+$lang['Name'] = 'Izena';
+$lang['Name is mandatory'] = 'Izena gehitzea derrigorrezkoa da';
+$lang['New e-mail'] = 'Posta elektroniko berria';
+$lang['No active email address'] = 'Ez dago helbide elektroniko aktiborik';
