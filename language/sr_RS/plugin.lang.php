c2fb194a54009550280859a35233e9415e412786
Piwigo-TranslationTeam <translate-github@piwigo.org>
2021-07-18 07:53:30 +0200

    Update sr_RS, thanks to gpopac

---
+++ language/sr_RS/plugin.lang.php
@@ -0,0 +1,46 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Plain text'] = 'Обичан текст';
+$lang['Please enter a message'] = 'Унесите поруку';
+$lang['Please enter a name'] = 'Унесите име';
+$lang['Please enter a subject'] = 'Унесите тему';
+$lang['Please enter an e-mail'] = 'Унесите е-пошту';
+$lang['Prefix of the sent e-mail subject'] = 'Префикс за тему послате е-поште';
+$lang['Redirect after sending email (optional)'] = 'Редирекција након слања поште (опционо)';
+$lang['Select a new user'] = 'Изаберите новог корисника';
+$lang['Send'] = 'Пошаљи';
+$lang['Send copy to my email'] = 'Пошаљи копију поруке на моју е-адресу';
+$lang['Subject'] = 'Тема';
+$lang['Text after the contact form'] = 'Текст након формулара за контакт';
+$lang['Text before the contact form'] = 'Текст пре формулара за контакт';
+$lang['This user has no e-mail'] = 'Овај корисник нема е-пошту';
+$lang['User already added'] = 'Корисник је већ додат';
+$lang['Your e-mail'] = 'Адреса ваше е-поште';
+$lang['Your name'] = 'Ваше име';
+$lang['can be translated with LocalFiles Editor plugin'] = 'може бити преведено помоћу додатка LocalFiles Editor';
+$lang['you can use "%gallery_title%"'] = 'можете корисити променљиву „%gallery_title%“';
+$lang['%s must not be less than %d characters long'] = '%s не сме бити краће од %d знака';
+$lang['%s must not be more than %d characters long'] = '%s не сме бити дуже од %d знака';
+$lang['Add link in menu'] = 'Додај везу у мени';
+$lang['Additional emails'] = 'Додатне е-поште';
+$lang['Allow guests to see the form'] = 'Дозволи гостима да виде формулар';
+$lang['Default e-mail subject'] = 'Подразумевани примаоц е-поште';
+$lang['E-mail address is mandatory'] = 'Адреса е-поште је обавезна';
+$lang['E-mail already added'] = 'Е-пошта је већ додата';
+$lang['E-mail configuration'] = 'Подешавање е-поште';
+$lang['E-mail format :'] = 'Формат е-поште:';
+$lang['E-mail sent successfully'] = 'Е-пошта је успешно послата';
+$lang['Error while sending e-mail'] = 'Грешка приликом слања е-поште';
+$lang['General configuration'] = 'Опште поставке';
+$lang['Invalid redirect URL'] = 'Неисправна редирекција везе';
+$lang['Manage contact e-mails'] = 'Управљање контактима е-поште';
+$lang['Message'] = 'Порука';
+$lang['Name'] = 'Име';
+$lang['Name is mandatory'] = 'Име је обавезно';
+$lang['New e-mail'] = 'Нова е-пошта';
+$lang['No active email address'] = 'Нема активних адреса е-поште';
