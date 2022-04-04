11fbcaa650f437cc5338899d54890b2f00ead041
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-04-04 06:38:26 +0200

    Update is_IS, thanks to gudjonsi

---
+++ language/is_IS/plugin.lang.php
@@ -0,0 +1,46 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Send'] = 'Senda';
+$lang['Send copy to my email'] = 'Sendu afrit á netfangið mitt';
+$lang['Subject'] = 'Efni';
+$lang['Text after the contact form'] = 'Texti á eftir tengslaeyðublaðinu';
+$lang['Text before the contact form'] = 'Texti á undan tengslaeyðublaðinu';
+$lang['This user has no e-mail'] = 'Þessi notandi hefur ekkert netfang';
+$lang['User already added'] = 'Notanda þegar bætt við';
+$lang['Your e-mail'] = 'Þitt netfang';
+$lang['Your name'] = 'Þitt nafn';
+$lang['can be translated with LocalFiles Editor plugin'] = 'hægt að þýða með LocalFiles Ritstjóraviðbót';
+$lang['you can use "%gallery_title%"'] = 'þú getur notað "%gallery_title%" breytuna';
+$lang['E-mail configuration'] = 'Tölvupóststillingar';
+$lang['E-mail format :'] = 'Tölvupóstsnið:';
+$lang['E-mail sent successfully'] = 'Tölvupóstur sendur';
+$lang['Error while sending e-mail'] = 'Villa við að senda tölvupóst';
+$lang['General configuration'] = 'Almenn uppsetning';
+$lang['Invalid redirect URL'] = 'Ógild tilvísunarslóð';
+$lang['Manage contact e-mails'] = 'Stjórna tengiliðatölvupósti';
+$lang['Message'] = 'Skilaboð';
+$lang['Name'] = 'Nafn';
+$lang['Name is mandatory'] = 'Nafn er skilyrt';
+$lang['New e-mail'] = 'Nýr tölvupóstur';
+$lang['No active email address'] = 'Ekkert virkt netfang';
+$lang['Plain text'] = 'Einfaldur texti';
+$lang['Please enter a message'] = 'Vinsamlegast sláðu inn skilaboð';
+$lang['Please enter a name'] = 'Vinsamlegast sláðu inn nafn';
+$lang['Please enter a subject'] = 'Vinsamlegast sláðu inn efni';
+$lang['Please enter an e-mail'] = 'Vinsamlegast sláðu inn netfang';
+$lang['Prefix of the sent e-mail subject'] = 'Forskeyti á efni sends tölvupósts';
+$lang['Redirect after sending email (optional)'] = 'Tilvísun eftir að hafa sent tölvupóst (valfrjálst)';
+$lang['Select a new user'] = 'Veldu nýjan notanda';
+$lang['E-mail already added'] = 'Tölvupósti þegar bætt við';
+$lang['E-mail address is mandatory'] = 'Netfang er skilyrt';
+$lang['Default e-mail subject'] = 'Sjálfgefið efni tölvupósts';
+$lang['Allow guests to see the form'] = 'Leyfðu gestum að sjá eyðublaðið';
+$lang['Additional emails'] = 'Fleiri tölvupóstar';
+$lang['Add link in menu'] = 'Bættu við hlekk í valmyndinni';
+$lang['%s must not be more than %d characters long'] = '%s má ekki vera meira en %d stafir að lengd';
+$lang['%s must not be less than %d characters long'] = '%s má ekki vera minna en %d stafir að lengd';
