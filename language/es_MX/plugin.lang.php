b6a0398f80dcec4390592ab2f7a2ffdab664b1e5
Piwigo-TranslationTeam <translate-github@piwigo.org>
2023-07-17 13:24:07 +0200

    Update es_MX, thanks to grantiago

---
+++ language/es_MX/plugin.lang.php
@@ -0,0 +1,46 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Plain text'] = 'Texto sin formato';
+$lang['Please enter a message'] = 'Por favor ingrese un mensaje';
+$lang['Please enter a name'] = 'Por favor ingresa un nombre';
+$lang['Please enter a subject'] = 'Por favor ingrese un ';
+$lang['Please enter an e-mail'] = 'Por favor ingrese un correo electrónico';
+$lang['Prefix of the sent e-mail subject'] = 'Prefijo del asunto del correo electrónico enviado';
+$lang['Redirect after sending email (optional)'] = 'Redirigir después de enviar un correo electrónico (opcional)';
+$lang['Select a new user'] = 'Seleccione un nuevo usuario';
+$lang['Send'] = 'Enviar';
+$lang['Send copy to my email'] = 'Enviar copia a mi correo electrónico';
+$lang['Subject'] = 'Asunto';
+$lang['Text after the contact form'] = 'Texto después del formulario de contacto';
+$lang['Text before the contact form'] = 'Texto antes del formulario de contacto';
+$lang['This user has no e-mail'] = 'Este usuario no tiene correo electrónico';
+$lang['User already added'] = 'Usuario ya agregado';
+$lang['Your e-mail'] = 'Tu correo electrónico';
+$lang['Your name'] = 'Su nombre';
+$lang['can be translated with LocalFiles Editor plugin'] = 'se puede traducir con el complemento LocalFiles Editor';
+$lang['you can use "%gallery_title%"'] = 'puedes usar la variable "%gallery_title%"';
+$lang['%s must not be less than %d characters long'] = '%s no debe tener menos de %d caracteres';
+$lang['%s must not be more than %d characters long'] = '%s no debe tener más de %d caracteres';
+$lang['Add link in menu'] = 'Añadir enlace en el menú';
+$lang['Additional emails'] = 'correos electrónicos adicionales';
+$lang['Allow guests to see the form'] = 'Permitir que los guests vean el formulario';
+$lang['Default e-mail subject'] = 'Asunto de correo electrónico predeterminado';
+$lang['E-mail address is mandatory'] = 'La dirección de correo electrónico es obligatoria';
+$lang['E-mail already added'] = 'Correo electrónico ya agregado';
+$lang['E-mail configuration'] = 'Configuración de correo electrónico';
+$lang['E-mail format :'] = 'Formato de Correo Electrónico :';
+$lang['E-mail sent successfully'] = 'Correo electrónico enviado con éxito';
+$lang['Error while sending e-mail'] = 'Error al enviar correo electrónico';
+$lang['General configuration'] = 'Configuración general';
+$lang['Invalid redirect URL'] = 'URL de redirección no válida';
+$lang['Manage contact e-mails'] = 'Administrar correos electrónicos de contacto';
+$lang['Message'] = 'Mensaje';
+$lang['Name'] = 'Nombre';
+$lang['Name is mandatory'] = 'El nombre es obligatorio';
+$lang['New e-mail'] = 'Nuevo Email';
+$lang['No active email address'] = 'Sin dirección de correo electrónico activa';
