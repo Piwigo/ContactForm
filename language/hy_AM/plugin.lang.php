8967b54c92f4b67cc4fddabf3f527c3d4775db6a
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-02-20 08:31:13 +0100

    Update hy_AM, thanks to gayane

---
+++ language/hy_AM/plugin.lang.php
@@ -0,0 +1,46 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Plain text'] = 'Պարզ տեքստ';
+$lang['Please enter a message'] = 'Խնդրում ենք մուտքագրել հաղորդագրությունը';
+$lang['Please enter a name'] = 'Խնդրում ենք մուտքագրել անունը';
+$lang['Please enter a subject'] = 'Խնդրում ենք մուտքագրել թեման';
+$lang['Please enter an e-mail'] = 'Խնդրում ենք մուտքագրել էլ. փոստը';
+$lang['Prefix of the sent e-mail subject'] = 'Ուղարկված էլ. փոստի թեմայի նախածանց';
+$lang['Redirect after sending email (optional)'] = 'Վերահղում էլ-նամակ ուղարկելուց հետո (ըստ ցանկության)';
+$lang['Select a new user'] = 'Ընտրել նոր օգտատեր';
+$lang['Send'] = 'Ուղարկել';
+$lang['Send copy to my email'] = 'Պատճենը ուղարկեք իմ էլ. հասցեին';
+$lang['Subject'] = 'Թեմա';
+$lang['Text after the contact form'] = 'Տեքստը կոնտակտային ձևից հետո';
+$lang['Text before the contact form'] = 'Տեքստը կոնտակտային ձևից առաջ';
+$lang['This user has no e-mail'] = 'Այս օգտատերը Էլ. փոստ չունի';
+$lang['User already added'] = 'Օգտատերն արդեն ավելացված է';
+$lang['Your e-mail'] = 'Ձեր Էլ. փոստը';
+$lang['Your name'] = 'Ձեր անունը';
+$lang['can be translated with LocalFiles Editor plugin'] = 'Կարող է թարգմանվել LocalFiles Editor հավելվածի միջոցով';
+$lang['you can use "%gallery_title%"'] = 'Կարող եք օգտագործել "%gallery_title%" փոփոխականը';
+$lang['%s must not be less than %d characters long'] = '%s երկարությունը չպետք է լինի %d նիշից պակաս';
+$lang['%s must not be more than %d characters long'] = '%s երկարությունը չպետք է գերազանցի %d նիշը';
+$lang['Add link in menu'] = 'Ավելացնել հղում մենյուի մեջ';
+$lang['Additional emails'] = 'Լրացուցիչ էլ. հասցե';
+$lang['Allow guests to see the form'] = 'Թույլ տալ հյուրերին տեսնել ձևը';
+$lang['Default e-mail subject'] = 'Էլ. փոստի կանխադրված թեմա';
+$lang['E-mail address is mandatory'] = 'Էլ. փոստի հասցեն պարտադիր է';
+$lang['E-mail already added'] = 'Էլ. փոստն արդեն ավելացված է';
+$lang['E-mail configuration'] = 'Էլ. փոստի կարգավորում';
+$lang['E-mail format :'] = 'Էլ. փոստի ձևաչափ:';
+$lang['E-mail sent successfully'] = 'Էլ. փոստը հաջողությամբ ուղարկված է';
+$lang['Error while sending e-mail'] = 'Սխալ էլ. նամակ ուղարկելիս';
+$lang['General configuration'] = 'Ընդհանուր կարգավորում';
+$lang['Invalid redirect URL'] = 'Անվավեր վերահղման URL';
+$lang['Manage contact e-mails'] = 'Կոնտակտային էլ. փոստի կառավարում';
+$lang['Message'] = 'Նամակ';
+$lang['Name'] = 'Անուն';
+$lang['Name is mandatory'] = 'Անունը պարտադիր է';
+$lang['New e-mail'] = 'Նոր էլ. փոստ';
+$lang['No active email address'] = 'Ակտիվ էլ. փոստի հասցե չկա';
