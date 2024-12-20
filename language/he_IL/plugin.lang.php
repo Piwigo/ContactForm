5c6997e633b29e87513b4bc98cc6facc44b7ab41
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-12-20 06:30:56 +0100

    Update he_IL, thanks to epifeny

---
+++ language/he_IL/plugin.lang.php
@@ -0,0 +1,46 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Plain text'] = 'טקסט פשוט';
+$lang['Please enter a message'] = 'אנא הכנס הודעה';
+$lang['Please enter a name'] = 'אנא הכנס שם';
+$lang['Please enter a subject'] = 'אנא הכנס נושא';
+$lang['Please enter an e-mail'] = 'אנא הכנס כתובת דוא"ל';
+$lang['Prefix of the sent e-mail subject'] = 'קידומת לנושא הדוא"ל הנשלח';
+$lang['Redirect after sending email (optional)'] = 'הפניה לאחר שליחת דוא"ל (אופציונלי)';
+$lang['Select a new user'] = 'בחר משתמש חדש';
+$lang['Send'] = 'שלח';
+$lang['Send copy to my email'] = 'שלח עותק לדוא"ל שלי';
+$lang['Subject'] = 'נושא';
+$lang['Text after the contact form'] = 'טקסט אחרי טופס יצירת הקשר';
+$lang['Text before the contact form'] = 'טקסט לפני טופס יצירת הקשר';
+$lang['This user has no e-mail'] = 'למשתמש זה אין כתובת דוא"ל';
+$lang['User already added'] = 'משתמש כבר נוסף';
+$lang['Your e-mail'] = 'כתובת הדוא"ל שלך';
+$lang['Your name'] = 'השם שלך';
+$lang['can be translated with LocalFiles Editor plugin'] = 'ניתן לתרגום באמצעות התוסף LocalFiles Editor';
+$lang['you can use "%gallery_title%"'] = 'ניתן להשתמש במשתנה "%gallery_title%"';
+$lang['%s must not be less than %d characters long'] = '%s לא יכול להיות קצר מ-%d תווים';
+$lang['%s must not be more than %d characters long'] = '%s לא יכול להיות ארוך מ-%d תווים';
+$lang['Add link in menu'] = 'הוסף קישור בתפריט';
+$lang['Additional emails'] = 'כתובות דוא"ל נוספות';
+$lang['Allow guests to see the form'] = 'אפשר לאורחים לראות את הטופס';
+$lang['Default e-mail subject'] = 'נושא ברירת המחדל לדוא"ל';
+$lang['E-mail address is mandatory'] = 'כתובת דוא"ל היא שדה חובה';
+$lang['E-mail already added'] = 'כתובת דוא"ל כבר נוספה';
+$lang['E-mail configuration'] = 'תצורת דוא"ל';
+$lang['E-mail format :'] = 'פורמט דוא"ל:';
+$lang['E-mail sent successfully'] = 'דוא"ל נשלח בהצלחה';
+$lang['Error while sending e-mail'] = 'שגיאה בעת שליחת דוא"ל';
+$lang['General configuration'] = 'תצורה כללית';
+$lang['Invalid redirect URL'] = 'כתובת URL להפניה שגויה';
+$lang['Manage contact e-mails'] = 'ניהול כתובות דוא"ל ליצירת קשר';
+$lang['Message'] = 'הודעה';
+$lang['Name'] = 'שם';
+$lang['Name is mandatory'] = 'שם הוא שדה חובה';
+$lang['New e-mail'] = 'דוא"ל חדש';
+$lang['No active email address'] = 'אין כתובת דוא"ל פעילה';
