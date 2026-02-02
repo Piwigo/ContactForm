1209b9ce1a5794be0614cfaa2e93392164a7ed07
Piwigo-TranslationTeam <translate-github@piwigo.org>
2026-02-02 11:45:06 +0100

    Update ko_KR, thanks to yjchoi

---
+++ language/ko_KR/plugin.lang.php
@@ -0,0 +1,46 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['you can use "%gallery_title%"'] = '"%gallery_title%" 변수를 사용할 수 있습니다';
+$lang['Plain text'] = '일반 텍스트';
+$lang['Please enter a message'] = '메시지를 입력하세요';
+$lang['Please enter a name'] = '이름을 입력하세요';
+$lang['Please enter a subject'] = '제목을 입력하세요';
+$lang['Please enter an e-mail'] = '이메일을 입력하세요';
+$lang['Prefix of the sent e-mail subject'] = '발송 이메일 제목 머리말';
+$lang['Redirect after sending email (optional)'] = '이메일 발송 후 리디렉션 (선택 사항)';
+$lang['Select a new user'] = '새 사용자 선택';
+$lang['Send'] = '보내기';
+$lang['Send copy to my email'] = '내 이메일로 사본 보내기';
+$lang['Subject'] = '제목';
+$lang['Text after the contact form'] = '문의 양식 후 텍스트';
+$lang['Text before the contact form'] = '문의 양식 전 텍스트';
+$lang['This user has no e-mail'] = '이 사용자는 이메일이 없습니다';
+$lang['User already added'] = '사용자가 이미 추가되었습니다';
+$lang['Your e-mail'] = '귀하의 이메일';
+$lang['Your name'] = '귀하의 이름';
+$lang['can be translated with LocalFiles Editor plugin'] = 'LocalFiles Editor 플러그인으로 번역 가능';
+$lang['%s must not be less than %d characters long'] = '%s는 %d자 이상이어야 합니다';
+$lang['%s must not be more than %d characters long'] = '%s는 %d자를 초과할 수 없습니다';
+$lang['Add link in menu'] = '메뉴에 링크 추가';
+$lang['Additional emails'] = '추가 이메일';
+$lang['Allow guests to see the form'] = '방문자가 양식을 볼 수 있도록 허용';
+$lang['Default e-mail subject'] = '기본 이메일 제목';
+$lang['E-mail address is mandatory'] = '이메일 주소는 필수 항목입니다';
+$lang['E-mail already added'] = '이메일은 이미 추가되었습니다';
+$lang['E-mail configuration'] = '이메일 설정';
+$lang['E-mail format :'] = '이메일 형식 :';
+$lang['E-mail sent successfully'] = '이메일이 성공적으로 발송되었습니다';
+$lang['Error while sending e-mail'] = '이메일 발송 중 오류 발생';
+$lang['General configuration'] = '일반 설정';
+$lang['Invalid redirect URL'] = '잘못된 리디렉션 URL';
+$lang['Manage contact e-mails'] = '연락처 이메일 관리';
+$lang['Message'] = '메시지';
+$lang['Name'] = '이름';
+$lang['Name is mandatory'] = '이름은 필수 항목입니다';
+$lang['New e-mail'] = '새 이메일';
+$lang['No active email address'] = '활성화된 이메일 주소가 없음';
